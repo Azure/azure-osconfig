ba9993c4bc1c73cc210a5e2d18706b88bc6f0a34
MariusNi <mariusni@microsoft.com>
2022-02-02 17:23:04 -0800

    Using local environment http proxy options for connecting to the IoT Hub

--- src/common/commonutils/CommonUtils.c
+++ src/common/commonutils/CommonUtils.c
@@ -662,7 +662,7 @@ bool ParseHttpProxyData(const char* proxyData, char** proxyHostAddress, int* pro
                 OsConfigLogInfo(log, "HTTP proxy host|address: %s (%d)", *proxyHostAddress, hostAddressLength);
                 OsConfigLogInfo(log, "HTTP proxy port: %d (%s, %d)", *proxyPort, port, portLength);
                 OsConfigLogInfo(log, "HTTP proxy username: %s (%d)", *proxyUsername, usernameLength);
-                OsConfigLogInfo(log, "HTTP proxy password: %s (%d)", IsFullLoggingEnabled() ? (*password) : "***", passwordLength);
+                OsConfigLogInfo(log, "HTTP proxy password: %s (%d)", (IsFullLoggingEnabled() ? (*proxyPassword) : "***"), passwordLength);
 
                 // Port is unused past this, can be freed; the rest must remain allocated
                 if (port)
