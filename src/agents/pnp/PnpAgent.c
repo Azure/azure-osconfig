77fac1025d8ccad6c246774458158de3d5996477
MariusNi <mariusni@microsoft.com>
2022-02-11 15:41:47 -0800

    Queueing for later desired twin payload to not block during twin callback

--- src/agents/pnp/PnpAgent.c
+++ src/agents/pnp/PnpAgent.c
@@ -861,15 +861,15 @@ void AgentDoWork(void)
 {
     tickcounter_ms_t nowTick = 0;
     tickcounter_ms_t intervalTick = g_reportingInterval * 1000;
+
     tickcounter_get_current_ms(g_tickCounter, &nowTick);
 
     if ((nowTick == g_lastTick) || (intervalTick <= (nowTick - g_lastTick)))
     {
         ProcessDesiredTwinUpdates();
-        
-        ReportProperties();
-
         LoadDesiredConfigurationFromFile();
+
+        ReportProperties();
         SaveReportedConfigurationToFile();
 
         CallMpiDoWork();
