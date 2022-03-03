078b56f21374a94d9638d8fcd50810745ca6e9a8
Robert Schaedler III <rschaedler@microsoft.com>
2022-03-02 22:33:55 -0500

    Refactor E2ETest base class (#101)
    * create refactored E2ETest base class
    
    * finalize refactored teardown
    
    * remove teardown until complete integration with other tests
    
    * update condition to wait for reported ack code
    
    * remove partial from class definitions
    
    * replace E2ETest base class with refactored test fixture
    
    * update tests to use the revised base class interface
    
    * add docstrings
    
    * remove GetReported variant for non-blocking condition
    
    * remove exceptions from ExecuteViaCommandRunner()
    
    * use const ack values
    
    * fix tpm test
    
    * fix ztsi tests
    
    * update variable naming
    
    * use env for default wait time for twin updates
    
    * remove ack code checks for settings module

--- src/tests/e2etest/CommandRunnerTests.cs
+++ src/tests/e2etest/CommandRunnerTests.cs
@@ -3,16 +3,15 @@
 
 using NUnit.Framework;
 using System;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace E2eTesting
 {
-    public class CommandRunnerTests : E2eTest
+    [TestFixture, Category("CommandRunner")]
+    public class CommandRunnerTests : E2ETest
     {
-        private const string componentName = "CommandRunner";
-        private const string commandArguments = "CommandArguments";
-        private const string commandStatus = "CommandStatus";
+        private static readonly string _componentName = "CommandRunner";
+        private static readonly string _desiredObjectName = "CommandArguments";
+        private static readonly string _reportedObjectName = "CommandStatus";
 
         public enum Action
         {
@@ -34,7 +33,7 @@ namespace E2eTesting
             Canceled
         }
 
-        public partial class CommandArguments
+        public class CommandArguments
         {
             public string CommandId { get; set; }
             public string Arguments { get; set; }
@@ -43,71 +42,25 @@ namespace E2eTesting
             public bool SingleLineTextResult { get; set; }
         }
 
-        public partial class CommandArgumentsResponse
-        {
-            public CommandArguments value { get; set; }
-            public int ac { get; set; }
-        }
-
-        public partial class CommandStatus
+        public class CommandStatus
         {
             public string CommandId { get; set; }
             public long ResultCode { get; set; }
             public string TextResult { get; set; }
             public CommandState CurrentState { get; set; }
         }
-        public CommandStatus WaitForCommandStatus(string commandId, CommandState commandState, int maxWaitSeconds = 180)
+
+        public static string GenerateId()
         {
-            var reportedCommandStatus = JsonSerializer.Deserialize<CommandStatus>(GetTwin().Properties.Reported[componentName][commandStatus].ToString());
-            DateTime startTime = DateTime.Now;
-
-            // Wait until the commandId is present in reported properties and the command is in the expected state
-            while((reportedCommandStatus.CommandId != commandId) || (reportedCommandStatus.CurrentState != commandState))
-            {
-                if ((DateTime.Now - startTime).TotalSeconds < maxWaitSeconds)
-                {
-                    Console.WriteLine("[CommandRunnerTests] waiting for {0} to complete...", commandId);
-                    Task.Delay(twinRefreshIntervalMs).Wait();
-                    reportedCommandStatus = JsonSerializer.Deserialize<CommandStatus>(GetNewTwin().Properties.Reported[componentName][commandStatus].ToString());
-                }
-                else
-                {
-                    Assert.Fail("Timed out while waiting for '{0}' to reach a '{1}' state, recieved: {2}", commandId, commandState, JsonSerializer.Serialize<CommandStatus>(reportedCommandStatus));
-                    break;
-                }
-            }
-
-            return reportedCommandStatus;
+            return Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 4);
         }
 
-        public CommandArguments CreateLongRunningCommand(string commandId, int timeout = 120)
+        public static CommandArguments CreateCommand(string arguments, Action action = Action.RunCommand, int timeout = 0, bool singleLineTextResult = false)
         {
-            return new CommandArguments
-            {
-                CommandId = commandId,
-                Arguments = "sleep 1000s",
-                Action = Action.RunCommand,
-                Timeout = timeout
-            };
+            return CreateCommand(GenerateId(), arguments, action, timeout, singleLineTextResult);
         }
 
-        public CommandArguments CreateCancelCommand(string commandId)
-        {
-            return new CommandArguments
-            {
-                CommandId = commandId,
-                Arguments = "",
-                Action = Action.CancelCommand,
-                Timeout = 0
-            };
-        }
-
-        public CommandArguments CreateCommand(string arguments, Action action = Action.RunCommand, int timeout = 0, bool singleLineTextResult = false)
-        {
-            return CreateCommand(CreateCommandId(), arguments, action, timeout, singleLineTextResult);
-        }
-
-        public CommandArguments CreateCommand(string commandId, string arguments, Action action = Action.RunCommand, int timeout = 0, bool singleLinetextResult = false)
+        public static CommandArguments CreateCommand(string commandId, string arguments, Action action = Action.RunCommand, int timeout = 0, bool singleLinetextResult = false)
         {
             return new CommandArguments
             {
@@ -119,7 +72,29 @@ namespace E2eTesting
             };
         }
 
-        public CommandStatus CreateCommandStatus(string commandId, string textResult = "", CommandState commandState = CommandState.Succeeded, int resultCode = 0)
+        public static CommandArguments CreateLongRunningCommand(string commandId, int timeout = 120)
+        {
+            return new CommandArguments
+            {
+                CommandId = commandId,
+                Arguments = "sleep 1000s",
+                Action = Action.RunCommand,
+                Timeout = timeout
+            };
+        }
+
+        public static CommandArguments CreateCancelCommand(string commandId)
+        {
+            return new CommandArguments
+            {
+                CommandId = commandId,
+                Arguments = "",
+                Action = Action.CancelCommand,
+                Timeout = 0
+            };
+        }
+
+        public static CommandStatus CreateCommandStatus(string commandId, string textResult = "", CommandState commandState = CommandState.Succeeded, int resultCode = 0)
         {
             return new CommandStatus
             {
@@ -130,23 +105,26 @@ namespace E2eTesting
             };
         }
 
-        public string CreateCommandId()
+        public void SendCommand(CommandArguments command, int expectedAckCode = ACK_SUCCESS)
         {
-            return Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 4);
-        }
+            int ackCode = -1;
 
-        public void SendCommand(CommandArguments command, int responseCode = 200)
-        {
-            var twinPatch = CreateCommandArgumentsPropertyPatch(componentName, command);
-            if (UpdateTwinBlockUntilUpdate(twinPatch))
+            Console.WriteLine($"{command.Action} \"{command.CommandId}\" ({command.Arguments})");
+
+            try
             {
-                var response = JsonSerializer.Deserialize<CommandArgumentsResponse>(GetTwin().Properties.Reported[componentName][commandArguments].ToString());
-                Assert.AreEqual(response.ac, responseCode);
-                // AreEqualByJson(response.value, command);
+                var setDesiredTask = SetDesired<CommandArguments>(_componentName, _desiredObjectName, command);
+                setDesiredTask.Wait();
+                ackCode = setDesiredTask.Result.ac;
             }
-            else
+            catch (Exception e)
             {
-                Assert.Fail("Timed out while waiting for twin update");
+                Assert.Fail("Failed to send command: {0}", e.Message);
+            }
+
+            if (ackCode != expectedAckCode)
+            {
+                Assert.Fail("CommandRunner.CommandArguments expected ackCode {0}, but got {1}", expectedAckCode, ackCode);
             }
         }
 
@@ -155,7 +133,7 @@ namespace E2eTesting
             SendCommand(CreateCancelCommand(commandId));
         }
 
-        public void RefreshCommandStatus(string commandId, int responseCode = 200)
+        public void RefreshCommandStatus(string commandId, int expectedAckCode = ACK_SUCCESS)
         {
             var refreshCommand = new CommandArguments
             {
@@ -164,96 +142,105 @@ namespace E2eTesting
                 Action = Action.RefreshCommandStatus
             };
 
-            SendCommand(refreshCommand, responseCode);
+            SendCommand(refreshCommand, expectedAckCode);
+        }
+
+        public CommandStatus WaitForStatus(string commandId, CommandState state)
+        {
+            Func<CommandStatus, bool> condition = (CommandStatus status) => ((status.CommandId == commandId) && (status.CurrentState == state));
+            var reportedTask = GetReported<CommandStatus>(_componentName, _reportedObjectName, condition);
+            reportedTask.Wait();
+            return reportedTask.Result;
         }
 
         [Test]
-        [TestCase("echo 'hello world'", 60, false, 0, "hello world\n", CommandState.Succeeded)]
-        [TestCase("sleep 30s", 1, true, 62, "", CommandState.TimedOut)]
-        [TestCase("sleep 10s", 0, true, 0, "", CommandState.Succeeded)]
+        [TestCase("echo 'hello world'", 0, false, 0, "hello world\n", CommandState.Succeeded)]
+        [TestCase("sleep 10s", 1, true, 62, "", CommandState.TimedOut)]
+        [TestCase("sleep 10s", 60, true, 0, "", CommandState.Succeeded)]
         [TestCase("echo 'single\nline'", 0, true, 0, "single line ", CommandState.Succeeded)]
         [TestCase("echo 'multiple\nlines'", 0, false, 0, "multiple\nlines\n", CommandState.Succeeded)]
         [TestCase("blah", 0, false, 127, "sh: 1: blah: not found\n", CommandState.Failed)]
-        public void CommandRunnerTest_RunCommand(string arguments, int timeout, bool singleLineTextResult, int resultCode, string textResult, CommandState commandState)
+        public void CommandRunnerTest_RunCommand(string arguments, int timeout, bool singleLineTextResult, int resultCode, string textResult, CommandState state)
         {
             var command = CreateCommand(arguments, Action.RunCommand, timeout, singleLineTextResult);
             SendCommand(command);
 
-            var commandStatus = WaitForCommandStatus(command.CommandId, commandState);
-            AreEqualByJson(CreateCommandStatus(command.CommandId, textResult, commandState, resultCode), commandStatus);
+            CommandStatus status = WaitForStatus(command.CommandId, state);
+            JsonAssert.AreEqual(CreateCommandStatus(command.CommandId, textResult, state, resultCode), status);
         }
 
         [Test]
         public void CommandRunnerTest_CancelCommand()
         {
-            var commandId = CreateCommandId();
+            var commandId = GenerateId();
             var command = CreateLongRunningCommand(commandId);
 
             SendCommand(command);
-            var runningCommandStatus = WaitForCommandStatus(commandId, CommandState.Running);
-            AreEqualByJson(CreateCommandStatus(commandId, "", CommandState.Running, 0), runningCommandStatus);
+            CommandStatus runningStatus = WaitForStatus(commandId, CommandState.Running);
 
             CancelCommand(command.CommandId);
-            var canceledCommandStatus = WaitForCommandStatus(commandId, CommandState.Canceled);
-            AreEqualByJson(CreateCommandStatus(commandId, "", CommandState.Canceled, 125), canceledCommandStatus);
+            CommandStatus canceledStatus = WaitForStatus(commandId, CommandState.Canceled);
+
+            Assert.Multiple(() =>
+            {
+                JsonAssert.AreEqual(CreateCommandStatus(commandId, "", CommandState.Running, 0), runningStatus);
+                JsonAssert.AreEqual(CreateCommandStatus(commandId, "", CommandState.Canceled, 125), canceledStatus);
+            });
         }
 
         [Test]
         public void CommandRunnerTest_RepeatCommandId()
         {
-            var commandId = CreateCommandId();
+            var commandId = GenerateId();
             var command = CreateCommand(commandId, "echo 'command 1'", Action.RunCommand, 0, true);
             var commandWithDuplicateCommandId = CreateCommand(commandId, "echo 'command 2'", Action.RunCommand, 0, true);
 
             SendCommand(command);
-            var commandStatus = WaitForCommandStatus(commandId, CommandState.Succeeded);
-            AreEqualByJson(CreateCommandStatus(commandId, "command 1 ", CommandState.Succeeded, 0), commandStatus);
+            var commandStatus = WaitForStatus(commandId, CommandState.Succeeded);
+            JsonAssert.AreEqual(CreateCommandStatus(commandId, "command 1 ", CommandState.Succeeded, 0), commandStatus);
 
             // Send a command with a duplicate command Id
-            var twinPatch = CreateCommandArgumentsPropertyPatch(componentName, commandWithDuplicateCommandId);
-            UpdateTwinBlockUntilUpdate(twinPatch);
-            var response = JsonSerializer.Deserialize<CommandArgumentsResponse>(GetTwin().Properties.Reported[componentName][commandArguments].ToString());
-            Assert.AreEqual(response.ac, 400);
-            AreEqualByJson(response.value, commandWithDuplicateCommandId);
+            SendCommand(commandWithDuplicateCommandId, 400);
         }
 
         [Test]
         public void CommandRunnerTest_CommandSequence()
         {
-            var command1 = CreateCommand("sleep 100s && echo 'command 1'");
+            var command1 = CreateLongRunningCommand(GenerateId());
             var command2 = CreateCommand("echo 'command 2'");
-            var command3 = CreateCommand("sleep 1000s && echo 'command 3'", timeout: 1);
+            var command3 = CreateCommand("sleep 10s && echo 'command 3'", timeout: 1);
             var command4 = CreateCommand("blah");
 
-            var expectedCommandStatus1 = CreateCommandStatus(command1.CommandId, "command 1\n");
-            var expectedCommandStatus2 = CreateCommandStatus(command2.CommandId, "", CommandState.Canceled, 125);
+            var expectedCommandStatus1 = CreateCommandStatus(command1.CommandId, "", CommandState.Canceled, 125);
+            var expectedCommandStatus2 = CreateCommandStatus(command2.CommandId, "command 2\n", CommandState.Succeeded, 0);
             var expectedCommandStatus3 = CreateCommandStatus(command3.CommandId, "", CommandState.TimedOut, 62);
             var expectedCommandStatus4 = CreateCommandStatus(command4.CommandId, "sh: 1: blah: not found\n", CommandState.Failed, 127);
 
             SendCommand(command1);
             SendCommand(command2);
-            SendCommand(CreateCancelCommand(command2.CommandId));
             SendCommand(command3);
             SendCommand(command4);
+            SendCommand(CreateCancelCommand(command1.CommandId));
 
             // Wait for the last command to complete before checking all command statuses
-            WaitForCommandStatus(command4.CommandId, CommandState.Failed);
+            CommandStatus actualCommandStatus4 = WaitForStatus(command4.CommandId, CommandState.Failed);
 
             RefreshCommandStatus(command1.CommandId);
-            var actualCommandStatus1 = WaitForCommandStatus(command1.CommandId, CommandState.Succeeded);
-            AreEqualByJson(expectedCommandStatus1, actualCommandStatus1);
+            CommandStatus actualCommandStatus1 = WaitForStatus(command1.CommandId, CommandState.Canceled);
 
             RefreshCommandStatus(command2.CommandId);
-            var actualCommandStatus2 = WaitForCommandStatus(command2.CommandId, CommandState.Canceled);
-            AreEqualByJson(expectedCommandStatus2, actualCommandStatus2);
+            CommandStatus actualCommandStatus2 = WaitForStatus(command2.CommandId, CommandState.Succeeded);
 
             RefreshCommandStatus(command3.CommandId);
-            var actualCommandStatus3 = WaitForCommandStatus(command3.CommandId, CommandState.TimedOut);
-            AreEqualByJson(expectedCommandStatus3, actualCommandStatus3);
+            CommandStatus actualCommandStatus3 = WaitForStatus(command3.CommandId, CommandState.TimedOut);
 
-            RefreshCommandStatus(command4.CommandId);
-            var actualCommandStatus4 = WaitForCommandStatus(command4.CommandId, CommandState.Failed);
-            AreEqualByJson(expectedCommandStatus4, actualCommandStatus4);
+            Assert.Multiple(() =>
+            {
+                JsonAssert.AreEqual(expectedCommandStatus1, actualCommandStatus1);
+                JsonAssert.AreEqual(expectedCommandStatus2, actualCommandStatus2);
+                JsonAssert.AreEqual(expectedCommandStatus3, actualCommandStatus3);
+                JsonAssert.AreEqual(expectedCommandStatus4, actualCommandStatus4);
+            });
         }
     }
 }
