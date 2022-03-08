4c1fc2412a38054837ba9f9b81f23858de7a913e
Robert Schaedler <rschaedler@microsoft.com>
2022-03-08 00:10:05 +0000

    add comment for creating max size limit for removed map elements

--- src/modules/samples/cpp/src/lib/Sample.h
+++ src/modules/samples/cpp/src/lib/Sample.h
@@ -62,6 +62,7 @@ public:
         std::map<std::string, int> integerMapSetting;
 
         // Store removed elements to report as 'null'
+        // These vectors should have a maximum size depending on the max payload size recieved by MmiOpen()
         std::vector<std::string> removedStringMapSettingKeys;
         std::vector<std::string> removedIntegerMapSettingKeys;
     };
