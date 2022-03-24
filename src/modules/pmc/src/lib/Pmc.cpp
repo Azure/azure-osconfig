aba0b2466d940945f0bab20e761a8933c4b5e289
Magda Baran <mbaran@microsoft.com>
2022-03-25 00:19:47 +0100

    Add PackageManagerConfiguration module skeleton (#137)
    Co-authored-by: Marius Niculescu <mariusni@microsoft.com>

---
+++ src/modules/pmc/src/lib/Pmc.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "Pmc.h"
+
+Pmc::Pmc(unsigned int maxPayloadSizeBytes)
+    : PmcBase(maxPayloadSizeBytes)
+{
+}
---
+++ src/modules/pmc/src/so/PmcModule.cpp
@@ -0,0 +1,210 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <cstdio>
+#include <cstring>
+
+#include <Pmc.h>
+#include <ScopeGuard.h>
+#include <Mmi.h>
+
+void __attribute__((constructor)) InitModule()
+{
+    PmcLog::OpenLog();
+    OsConfigLogInfo(PmcLog::Get(), "C++ PackageManagerConfiguration module loaded");
+}
+
+void __attribute__((destructor)) DestroyModule()
+{
+    OsConfigLogInfo(PmcLog::Get(), "C++ PackageManagerConfiguration module unloaded");
+    PmcLog::CloseLog();
+}
+
+int MmiGetInfo(
+    const char* clientName,
+    MMI_JSON_STRING* payload,
+    int* payloadSizeBytes)
+{
+    int status = MMI_OK;
+
+    ScopeGuard sg{[&]()
+    {
+        if (MMI_OK == status)
+        {
+            if (IsFullLoggingEnabled())
+            {
+                OsConfigLogInfo(PmcLog::Get(), "MmiGetInfo(%s, %.*s, %d) returned %d", clientName, *payloadSizeBytes, *payload, *payloadSizeBytes, status);
+            }
+            else
+            {
+                OsConfigLogInfo(PmcLog::Get(), "MmiGetInfo(%s, -, %d) returned %d", clientName, *payloadSizeBytes, status);
+            }
+        }
+        else
+        {
+            if (IsFullLoggingEnabled())
+            {
+                OsConfigLogError(PmcLog::Get(), "MmiGetInfo(%s, %.*s, %d) returned %d", clientName, *payloadSizeBytes, *payload, *payloadSizeBytes, status);
+            }
+            else
+            {
+                OsConfigLogError(PmcLog::Get(), "MmiGetInfo(%s, -, %d) returned %d", clientName, *payloadSizeBytes, status);
+            }
+        }
+    }};
+
+    status = Pmc::GetInfo(clientName, payload, payloadSizeBytes);
+
+    return status;
+}
+
+MMI_HANDLE MmiOpen(
+    const char* clientName,
+    const unsigned int maxPayloadSizeBytes)
+{
+    int status = MMI_OK;
+    MMI_HANDLE handle = nullptr;
+
+    ScopeGuard sg{[&]()
+    {
+        if (MMI_OK == status)
+        {
+            OsConfigLogInfo(PmcLog::Get(), "MmiOpen(%s, %d) returned: %p, status: %d", clientName, maxPayloadSizeBytes, handle, status);
+        }
+        else
+        {
+            OsConfigLogError(PmcLog::Get(), "MmiOpen(%s, %d) returned: %p, status: %d", clientName, maxPayloadSizeBytes, handle, status);
+        }
+    }};
+
+    if (nullptr != clientName)
+    {
+        try
+        {
+            Pmc* session = new (std::nothrow) Pmc(maxPayloadSizeBytes);
+            if (nullptr == session)
+            {
+                OsConfigLogError(PmcLog::Get(), "MmiOpen failed to allocate memory");
+                status = ENOMEM;
+            }
+            else
+            {
+                handle = reinterpret_cast<MMI_HANDLE>(session);
+            }
+        }
+        catch (std::exception& e)
+        {
+            OsConfigLogError(PmcLog::Get(), "MmiOpen exception thrown: %s", e.what());
+            status = EINTR;
+        }
+    }
+    else
+    {
+        OsConfigLogError(PmcLog::Get(), "MmiOpen called with null clientName");
+        status = EINVAL;
+    }
+
+    return handle;
+}
+
+void MmiClose(MMI_HANDLE clientSession)
+{
+    Pmc* session = reinterpret_cast<Pmc*>(clientSession);
+    if (nullptr != session)
+    {
+        delete session;
+    }
+}
+
+int MmiSet(
+    MMI_HANDLE clientSession,
+    const char* componentName,
+    const char* objectName,
+    const MMI_JSON_STRING payload,
+    const int payloadSizeBytes)
+{
+    int status = MMI_OK;
+    Pmc* session = nullptr;
+
+    ScopeGuard sg{[&]()
+    {
+        if (MMI_OK == status)
+        {
+            if (IsFullLoggingEnabled())
+            {
+                OsConfigLogInfo(PmcLog::Get(), "MmiSet(%p, %s, %s, %.*s, %d) returned %d", clientSession, componentName, objectName, payloadSizeBytes, payload, payloadSizeBytes, status);
+            }
+        }
+        else
+        {
+            if (IsFullLoggingEnabled())
+            {
+                OsConfigLogError(PmcLog::Get(), "MmiSet(%p, %s, %s, %.*s, %d) returned %d", clientSession, componentName, objectName, payloadSizeBytes, payload, payloadSizeBytes, status);
+            }
+            else
+            {
+                OsConfigLogError(PmcLog::Get(), "MmiSet(%p, %s, %s, -, %d) returned %d", clientSession, componentName, objectName, payloadSizeBytes, status);
+            }
+        }
+    }};
+
+    if (nullptr == clientSession)
+    {
+        OsConfigLogError(PmcLog::Get(), "MmiSet called with null clientSession");
+        status = EINVAL;
+    }
+    else
+    {
+        session = reinterpret_cast<Pmc*>(clientSession);
+        status = session->Set(componentName, objectName, payload, payloadSizeBytes);
+    }
+
+    return status;
+}
+
+int MmiGet(
+    MMI_HANDLE clientSession,
+    const char* componentName,
+    const char* objectName,
+    MMI_JSON_STRING* payload,
+    int* payloadSizeBytes)
+{
+    int status = MMI_OK;
+    Pmc* session = nullptr;
+
+    ScopeGuard sg{[&]()
+    {
+        if (IsFullLoggingEnabled())
+        {
+            if (MMI_OK == status)
+            {
+                OsConfigLogInfo(PmcLog::Get(), "MmiGet(%p, %s, %s, %.*s, %d) returned %d", clientSession, componentName, objectName, *payloadSizeBytes, *payload, *payloadSizeBytes, status);
+            }
+            else
+            {
+                OsConfigLogError(PmcLog::Get(), "MmiGet(%p, %s, %s, %.*s, %d) returned %d", clientSession, componentName, objectName, *payloadSizeBytes, *payload, *payloadSizeBytes, status);
+            }
+        }
+    }};
+
+    if (nullptr == clientSession)
+    {
+        OsConfigLogError(PmcLog::Get(), "MmiGet called with null clientSession");
+        status = EINVAL;
+    }
+    else
+    {
+        session = reinterpret_cast<Pmc*>(clientSession);
+        status = session->Get(componentName, objectName, payload, payloadSizeBytes);
+    }
+
+    return status;
+}
+
+void MmiFree(MMI_JSON_STRING payload)
+{
+    if (nullptr != payload)
+    {
+        delete[] payload;
+    }
+}
