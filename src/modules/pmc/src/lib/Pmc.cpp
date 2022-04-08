0066f0956767dcad53eddea9fd1ca0c67eac198c
Magda Baran <mbaran@microsoft.com>
2022-04-08 18:43:47 +0200

    Add sources logic to Pmc module and use HashCommand for computing fingerprints (#169)
    * Add logic for configuring sources in PMC module
    * Simplify ExecuteUpdates
    * Improve formatting in tests
    * Log info only when full logging enabled
    * Use HashString for computing payload hash
    * Delete unneeded LogInfo
    * Use PersistentHashString for computing fingerprints
    * Use HashCommand
    * Improve formatting
    * Do not use MMI_OK in non-MMI calls and improve format
    Co-authored-by: Marius Niculescu <mariusni@microsoft.com>

--- src/modules/pmc/src/lib/Pmc.cpp
+++ src/modules/pmc/src/lib/Pmc.cpp
@@ -1,10 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <regex>
 #include <string>
 #include <CommonUtils.h>
 #include <Mmi.h>
 #include <Pmc.h>
+
+constexpr const char* g_commandGetInstalledPackages = "dpkg-query --showformat='${Package} (=${Version})\n' --show";
+constexpr const char* g_commandGetSourcesContent = "find $value -type f -name '*.list' -exec cat {} \\;";
 
 Pmc::Pmc(unsigned int maxPayloadSizeBytes)
     : PmcBase(maxPayloadSizeBytes)
@@ -16,7 +20,7 @@ int Pmc::RunCommand(const char* command, std::string* textResult, bool isLongRun
     char* buffer = nullptr;
     int status = ExecuteCommand(nullptr, command, true, true, 0, isLongRunning ? TIMEOUT_LONG_RUNNING : 0, &buffer, nullptr, PmcLog::Get()); 
 
-    if (status == MMI_OK)
+    if (status == 0)
     {
         if (buffer && textResult)
         {
@@ -27,4 +31,30 @@ int Pmc::RunCommand(const char* command, std::string* textResult, bool isLongRun
     FREE_MEMORY(buffer);
 
     return status;
+}
+
+std::string Pmc::GetPackagesFingerprint()
+{
+    char* hash = HashCommand(g_commandGetInstalledPackages, PmcLog::Get());
+    std::string hashString = hash ? hash : "(failed)";
+    FREE_MEMORY(hash)
+    return hashString;
+}
+
+std::string Pmc::GetSourcesFingerprint(const char* sourcesDirectory)
+{
+    char* hash = nullptr;
+    if (FileExists(sourcesDirectory))
+    {
+        std::string command = std::regex_replace(g_commandGetSourcesContent, std::regex("\\$value"), sourcesDirectory);
+        hash = HashCommand(command.c_str(), PmcLog::Get());
+    }
+    else if (IsFullLoggingEnabled())
+    {
+        OsConfigLogError(PmcLog::Get(), "Unable to get the fingerprint of source files. Directory %s does not exist", sourcesDirectory);
+    }
+
+    std::string hashString = hash ? hash : "(failed)";
+    FREE_MEMORY(hash)
+    return hashString;
 }
