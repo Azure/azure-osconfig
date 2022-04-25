71bf2bbc9991eace94282bd3effe9d945cc440b6
Marius Niculescu (216493) <mariusni@microsoft.com>
2022-04-25 14:30:50 -0700

    First incomplete draft

--- src/agents/pnp/MpiProxy.c
+++ src/agents/pnp/MpiProxy.c
@@ -21,6 +21,150 @@ extern MPI_HANDLE g_mpiHandle;
 char g_mpiCall[MPI_CALL_MESSAGE_LENGTH] = {0};
 static const char g_mpiCallTemplate[] = " during %s to %s.%s\n";
 
+static const char *g_requestBodyFormatMpiOpen = "{ \"Function\": \"MpiOpen\", \"Arguments\": { \"ClientName\": \"%s\", \"MaxPayloadSizeBytes\": %d } }";
+static const char *g_requestBodyFormatMpiClose = "{ \"Function\": \"MpiClose\", \"Arguments\": { \"ClientName\": \"%s\" } }";
+static const char *g_requestBodyFormatMpiGet = "{ \"Function\": \"MpiGet\", \"Arguments\": { \"ClientName\": \"%s\", \"ComponentName\": \"%s\", \"ObjectName\": \"%s\" } }";
+static const char *g_requestBodyFormatMpiSet = "{ \"Function\": \"MpiSet\", \"Arguments\": { \"ClientName\": \"%s\", \"ComponentName\": \"%s\", \"ObjectName\": \"%s\", \"Payload\": %s } }";
+static const char *g_requestBodyFormatMpiGetReported = "{ \"Function\": \"MpiGetReported\", \"Arguments\": { \"ClientName\": \"%s\" } }";
+static const char *g_requestBodyFormatMpiSetDesired = "{ \"Function\": \"MpiSetDesired\", \"Arguments\": { \"ClientName\": \"%s\", \"Payload\": %s } }";
+
+static const int g_bufferSize = 1024; //TODO replace this
+
+int CallMpi(const char* requestBody, char** response, int* responseSize)
+{
+    const char* mpiSocket = "/run/osconfig/platformd.sock";
+    const char* dataFormat = "POST /mpi HTTP/1.1\r\nHost: osconfig\r\nUser-Agent: osconfig\r\nAccept: */*\r\nContent-Type: application/json\r\nContent-Length: %d\r\n\r\n%s";
+    const char* terminator = "\r\n\r\n";
+
+    char data[g_bufferSize] = {0};
+    struct sockaddr_un socketAddress = {0};
+    socklen_t socketLength = 0;
+    int responseLength = 0;
+    int socket = -1;
+    ssize_t bytes = 0;
+    char* header = NULL;
+    int status = MPI_OK;
+
+    if ((NULL == requestBody) || (NULL == response) || (NULL == responseSize))
+    {
+        status = EINVAL;
+        OsConfigLogError(GetLog(), "CallMpi(%s, %p, %p) called with invalid arguments: %d", requestBody, response, responseSize, status);
+        return status;
+    }
+    
+    *response = NULL;
+    *responseSize = 0;
+
+    socketHandle = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (0 > socketHandle)
+    {
+        status = errno ? errno : EIO;
+        OsConfigLogError(GetLog(), "CallMpi(%s, %p, %p) failed to open socket: %d", requestBody, response, responseSize, status);
+    }
+    else
+    {
+        memset(&socketAddress, 0, sizeof(socketAddress));
+        socketAddress.sun_family = AF_UNIX;
+        strncpy(socketAddress.sun_path, mpiSocket, sizeof(socketAddress.sun_path) - 1);
+        socketLength = sizeof(socketAddress);
+    }
+
+    if (MPI_OK == status)
+    {
+        if (0 != connect(socketHandle, (struct socksocketAddress*)&socketAddress, socketLength))
+        {
+            status = errno ? errno : EIO;
+            OsConfigLogError(GetLog(), "CallMpi(%s, %p, %p) failed to connect to socket: %d", requestBody, response, responseSize, status);
+        }
+        else
+        {
+            snprintf(data, sizeof(data), dataFormat, strlen(requestBody), requestBody);
+        }
+    }
+
+    if (MPI_OK == status)
+    {
+        bytes = send(socketHandle, data, strlen(data), 0);
+        if (bytes != strlen(data))
+        {
+            status = errno ? errno : EIO;
+            OsConfigLogError(GetLog(), "CallMpi(%s, %p, %p) failed to send request to socket: %d", requestBody, response, responseSize, status);
+        }
+    }
+
+    if (MPI_OK == status)
+    {
+        memset(data, 0, sizeof(data));
+        bytes = 0;
+
+        bytes = read(socketHandle, data, sizeof(data));
+        if (0 < bytes)
+        {
+            status = errno ? errno : EIO;
+            OsConfigLogError(GetLog(), "CallMpi(%s, %p, %p) failed to read response from socket: %d", requestBody, response, responseSize, status);
+        }
+    }
+
+    if (MPI_OK == status)
+    {
+        if (header = strstr(buffer, "HTTP/1.1 20"))
+        {
+            if (header = strstr(buffer, terminator))
+            {
+                header += strlen(terminator);
+
+                responseLength = ((start > &buffer[0]) && (bytes > (start - &buffer[0])) ? (bytes - (start - &buffer[0])) : 0;
+                if (0 == responseLength)
+                {
+                    status = EIO;
+                    OsConfigLogError(GetLog(), "CallMpi(%s, %p, %p) invalid response: %d", requestBody, response, responseSize, status);
+                }
+                else
+                {
+                    *response = (char*)malloc(responseLength + 1);
+                    if (NULL == *response)
+                    {
+                        status = ENOMEM;
+                        OsConfigLogError(GetLog(), "CallMpi(%s, %p, %p) failed to allocate memory for response: %d", requestBody, response, responseSize, status);
+                    }
+                    else
+                    {
+                        memset(*response, 0, responseLength + 1);
+                        strncpy(*response, header, responseLength);
+                        *responseSize = responseLength;
+                    }
+                }
+            }
+            else
+            {
+                status = EIO;
+                OsConfigLogError(GetLog(), "CallMpi(%s, %p, %p) invalid response header: %d", requestBody, response, responseSize, status);
+            }
+        }
+        else
+        {
+            status = EIO;
+            OsConfigLogError(GetLog(), "CallMpi(%s, %p, %p) invalid response header: %d", requestBody, response, responseSize, status);
+        }
+    }
+
+    if (0 <= socketHandle)
+    {
+        close(socketHandle);
+    }
+
+    if (IsFullLoggingEnabled())
+    {
+        OsConfigLogInfo(GetLog(), "CallMpi(%s, %s, %s, %s, %d) returned %d", requestBody, *response, *responseSize);
+    }
+    else
+    {
+        OsConfigLogInfo(GetLog(), "CallMpi(%s, %s, %p, %p, %d) returned %d", (void*)requestBody, response, *responseSize);
+    }
+    
+    return status;
+}
+
 MPI_HANDLE CallMpiOpen(const char* clientName, const unsigned int maxPayloadSizeBytes)
 {
     MPI_HANDLE mpiHandle = MpiOpen(clientName, maxPayloadSizeBytes);
