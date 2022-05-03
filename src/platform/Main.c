e86b6205e3044351d3ed654a6216d8cc815aaf84
Marius Niculescu (216493) <mariusni@microsoft.com>
2022-05-03 14:54:47 -0700

    Temp tick counter

--- src/platform/Main.c
+++ src/platform/Main.c
@@ -15,6 +15,89 @@
 // The configuration file for OSConfig
 #define CONFIG_FILE "/etc/osconfig/osconfig.json"
 
+/////////////////////////////////////////////////////////
+#include <stdint.h>
+#include <time.h>
+#include <pthread.h>
+
+#ifndef __MACH__
+extern clockid_t time_basis;
+#endif
+
+extern void set_time_basis(void);
+extern int get_time_ns(struct timespec* ts);
+extern int64_t get_time_ms(void);
+
+#define INVALID_TIME_VALUE (int64_t)(-1)
+
+#define NANOSECONDS_IN_1_SECOND 1000000000L
+#define MILLISECONDS_IN_1_SECOND 1000
+#define NANOSECONDS_IN_1_MILLISECOND 1000000L
+
+typedef struct TICK_COUNTER_INSTANCE_TAG
+{
+    int64_t init_time_value;
+    tickcounter_ms_t current_ms;
+} TICK_COUNTER_INSTANCE;
+
+TICK_COUNTER_HANDLE tickcounter_create(void)
+{
+    TICK_COUNTER_INSTANCE* result = (TICK_COUNTER_INSTANCE*)malloc(sizeof(TICK_COUNTER_INSTANCE));
+    if (result != NULL)
+    {
+        set_time_basis();
+
+        result->init_time_value = get_time_ms();
+        if (result->init_time_value == INVALID_TIME_VALUE)
+        {
+            free(result);
+            result = NULL;
+        }
+        else
+        {
+            result->current_ms = 0;
+        }
+    }
+    return result;
+}
+
+void tickcounter_destroy(TICK_COUNTER_HANDLE tick_counter)
+{
+    if (tick_counter != NULL)
+    {
+        free(tick_counter);
+    }
+}
+
+int tickcounter_get_current_ms(TICK_COUNTER_HANDLE tick_counter, tickcounter_ms_t * current_ms)
+{
+    int result;
+
+    if (tick_counter == NULL || current_ms == NULL)
+    {
+        result = MU_FAILURE;
+    }
+    else
+    {
+        int64_t time_value = get_time_ms();
+        if (time_value == INVALID_TIME_VALUE)
+        {
+            result = MU_FAILURE;
+        }
+        else
+        {
+            TICK_COUNTER_INSTANCE* tick_counter_instance = (TICK_COUNTER_INSTANCE*)tick_counter;
+            tick_counter_instance->current_ms = (tickcounter_ms_t)time_value - tick_counter_instance->init_time_value;
+            *current_ms = tick_counter_instance->current_ms;
+            result = 0;
+        }
+    }
+
+    return result;
+}
+
+/////////////////////////////////////////////////////////
+
 static TICK_COUNTER_HANDLE g_tickCounter = NULL;
 static tickcounter_ms_t g_lastTick = 0;
 
