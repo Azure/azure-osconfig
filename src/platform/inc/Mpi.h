3a82f4c3f1dff3028ab79540b4cb2b0c2cd94697
Marius Niculescu <mariusni@ntdev.microsoft.com>
2022-05-04 12:42:43 -0700

    Adding back void

--- src/platform/inc/Mpi.h
+++ src/platform/inc/Mpi.h
@@ -44,7 +44,7 @@ void MpiClose(MPI_HANDLE clientSession);
 
 void MpiFree(MPI_JSON_STRING payload);
      
-void MpiInitialize();
+void MpiInitialize(void);
 void MpiDoWork(void);
 void MpiShutdown(void);
 
