b2681771e6727472ccbb3eded5eea1551ee7d31a
MariusNi <mariusni@microsoft.com>
2022-06-02 14:12:12 -0700

    More

--- src/modules/test/Common.h
+++ src/modules/test/Common.h
@@ -26,10 +26,23 @@
 #include <CommonUtils.h>
 #include <Mmi.h>
 
+// Use <filesystem> or <experimental/filesystem> based on gcc lib availability
+#if __has_include(<filesystem>)
+    #include <filesystem>
+#else
+    #include <experimental/filesystem>
+    
+    // Alias namespace
+    namespace std 
+    {
+        namespace filesystem = experimental::filesystem;
+    }
+#endif
+
 #include "ManagementModule.h"
-#include "MimParser.h"
-#include "RecipeInvoker.h"
 #include "TestRecipeParser.h"
+#include "RecipeInvoker.h"
+#include "MimParser.h"
 
 constexpr const size_t g_lineLength = 256;
 
@@ -51,18 +64,5 @@ inline void TestLogError(const char* format, const char* args...)
     std::snprintf(buf, g_lineLength, format, args);
     std::cerr << buf;
 }
-
-// Use <filesystem> or <experimental/filesystem> based on gcc lib availability
-#if __has_include(<filesystem>)
-    #include <filesystem>
-#else
-    #include <experimental/filesystem>
-    
-    // Alias namespace
-    namespace std 
-    {
-        namespace filesystem = experimental::filesystem;
-    }
-#endif
 
 #endif // MODULESTESTCOMMON_H
--- src/modules/test/unit-tests/TestRecipeParserTests.cpp
+++ src/modules/test/unit-tests/TestRecipeParserTests.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include <TestRecipeParser.h>
+#include <Common.h>
 
 namespace Tests
 {
