413b94c75ef7ea93f5b30ef899485fab6869767b
Ahmed Messaoud <ahbenmes@microsoft.com>
2022-06-21 12:51:06 -0700

    Added e2e cloud-init support (#271)

--- devops/scripts/install_deps_ubuntu2004.sh
+++ devops/scripts/install_deps_ubuntu2004.sh
@@ -7,4 +7,4 @@ apt -y update
 apt-get -y install git cmake build-essential curl libcurl4-openssl-dev libssl-dev uuid-dev libgtest-dev libgmock-dev liblttng-ust-dev rapidjson-dev ninja-build wget
 # Install Dotnet - Needed for e2e tests
 # See https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#1804-
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 6.0
