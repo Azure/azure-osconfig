351dc4a2e8e97697818fac3c62ade2405bb18e70
Harrison Chotzen <hachotze@microsoft.com>
2022-08-24 11:26:11 -0700

    Removing changes from RecipeInvoker

--- src/modules/test/RecipeInvoker.cpp
+++ src/modules/test/RecipeInvoker.cpp
@@ -96,8 +96,6 @@ void RecipeInvoker::TestBody()
         std::cout << "Waiting for " << m_recipe.m_waitSeconds << " seconds" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(m_recipe.m_waitSeconds));
     }
-
-    std::remove("/etc/osconfig/osconfig_commandrunner.cache");
 }
 
 void BasicModuleTester::TestBody()
