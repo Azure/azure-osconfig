ff7d3b69ad95b85894673d12fbcbf69da4af3347
Ahmed Messaoud <ahbenmes@microsoft.com>
2022-09-13 20:24:36 -0700

    Added model number to client name (#386)

--- src/modules/test/Common.h
+++ src/modules/test/Common.h
@@ -51,6 +51,47 @@
 #define TestLogInfo(format, ...) {printf(format, ##__VA_ARGS__); std::cout << std::endl;}
 #define TestLogError(format, ...) {fprintf(stderr, format, ##__VA_ARGS__); std::cerr << std::endl;}
 
-#define DefaultClientName "Azure OSConfig " OSCONFIG_VERSION
+#define DEFAULT_CLIENT_NAME "Azure OSConfig"
+#define MODEL_VERSION_NAME "ModelVersion"
+#define CONFIG_FILE "/etc/osconfig/osconfig.json"
+#define DEFAULT_DEVICE_MODEL_ID 11
+#define DEFAULT_FULL_CLIENT_NAME_TEMPLATE DEFAULT_CLIENT_NAME " %d;%s"
+#define CLIENT_NAME_MAX_SIZE 128
+
+static inline std::string GetFullClientName()
+{
+    JSON_Value* rootValue = NULL;
+    JSON_Object* rootObject = NULL;
+    int modelNumber = DEFAULT_DEVICE_MODEL_ID;
+
+    char* jsonConfiguration = LoadStringFromFile(CONFIG_FILE, false, NULL);
+    if (NULL != jsonConfiguration)
+    {
+        if (NULL != (rootValue = json_parse_string(jsonConfiguration)))
+        {
+            if (NULL != (rootObject = json_value_get_object(rootValue)))
+            {
+                modelNumber = (int)json_object_get_number(rootObject, MODEL_VERSION_NAME);
+            }
+            else
+            {
+                TestLogError("GetModelVersionFromJsonConfig: Failed to reveive value, using default (%d)", DEFAULT_DEVICE_MODEL_ID);
+            }
+            json_value_free(rootValue);
+        }
+        else
+        {
+            TestLogError("GetModelVersionFromJsonConfig: Failed to read json, using default (%d)", DEFAULT_DEVICE_MODEL_ID);
+        }
+    }
+    else
+    {
+        TestLogError("GetModelVersionFromJsonConfig: No configuration data, using default (%d)", DEFAULT_DEVICE_MODEL_ID);
+    }
+
+    char fullClientName[CLIENT_NAME_MAX_SIZE] = {0};
+    snprintf(fullClientName, sizeof(fullClientName), DEFAULT_FULL_CLIENT_NAME_TEMPLATE, modelNumber, OSCONFIG_VERSION);
+    return fullClientName;
+}
 
 #endif // MODULESTESTCOMMON_H
