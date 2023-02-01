893b5fb13951423800b7d540f115a51f03f0ed46
Marius Niculescu (216493) <mariusni@microsoft.com>
2023-02-01 13:44:44 -0800

    Drafting first basic framework for the Universal NRP for Machine Configuration

---
+++ src/adapters/mc/Common.h
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <parson.h>
+#include <CommonUtils.h>
+#include <Logging.h>
+#include <Mpi.h>
+#include <MpiClient.h>
+
+#include "sal.h"
+#include "MI.h"
+#include "MSFT_Credential.h"
+#include "OMI_BaseResource.h"
+#include "LinuxOsConfigResource.h"
+
+#endif
---
+++ src/adapters/mc/LinuxOsConfigResource.c
@@ -0,0 +1,167 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "Common.h"
+
+/* @migen@ */
+
+void MI_CALL LinuxOsConfigResource_Load(
+    _Outptr_result_maybenull_ LinuxOsConfigResource_Self** self,
+    _In_opt_ MI_Module_Self* selfModule,
+    _In_ MI_Context* context)
+{
+    MI_UNREFERENCED_PARAMETER(selfModule);
+
+    *self = NULL;
+    MI_Context_PostResult(context, MI_RESULT_OK);
+}
+
+void MI_CALL LinuxOsConfigResource_Unload(
+    _In_opt_ LinuxOsConfigResource_Self* self,
+    _In_ MI_Context* context)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+
+    MI_Context_PostResult(context, MI_RESULT_OK);
+}
+
+void MI_CALL LinuxOsConfigResource_EnumerateInstances(
+    _In_opt_ LinuxOsConfigResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_opt_ const MI_PropertySet* propertySet,
+    _In_ MI_Boolean keysOnly,
+    _In_opt_ const MI_Filter* filter)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(propertySet);
+    MI_UNREFERENCED_PARAMETER(keysOnly);
+    MI_UNREFERENCED_PARAMETER(filter);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL LinuxOsConfigResource_GetInstance(
+    _In_opt_ LinuxOsConfigResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_ const LinuxOsConfigResource* instanceName,
+    _In_opt_ const MI_PropertySet* propertySet)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(instanceName);
+    MI_UNREFERENCED_PARAMETER(propertySet);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL LinuxOsConfigResource_CreateInstance(
+    _In_opt_ LinuxOsConfigResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_ const LinuxOsConfigResource* newInstance)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(newInstance);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL LinuxOsConfigResource_ModifyInstance(
+    _In_opt_ LinuxOsConfigResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_ const LinuxOsConfigResource* modifiedInstance,
+    _In_opt_ const MI_PropertySet* propertySet)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(modifiedInstance);
+    MI_UNREFERENCED_PARAMETER(propertySet);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL LinuxOsConfigResource_DeleteInstance(
+    _In_opt_ LinuxOsConfigResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_ const LinuxOsConfigResource* instanceName)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(instanceName);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL LinuxOsConfigResource_Invoke_GetTargetResource(
+    _In_opt_ LinuxOsConfigResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_opt_z_ const MI_Char* methodName,
+    _In_ const LinuxOsConfigResource* instanceName,
+    _In_opt_ const LinuxOsConfigResource_GetTargetResource* in)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(methodName);
+    MI_UNREFERENCED_PARAMETER(instanceName);
+    MI_UNREFERENCED_PARAMETER(in);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL LinuxOsConfigResource_Invoke_TestTargetResource(
+    _In_opt_ LinuxOsConfigResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_opt_z_ const MI_Char* methodName,
+    _In_ const LinuxOsConfigResource* instanceName,
+    _In_opt_ const LinuxOsConfigResource_TestTargetResource* in)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(methodName);
+    MI_UNREFERENCED_PARAMETER(instanceName);
+    MI_UNREFERENCED_PARAMETER(in);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL LinuxOsConfigResource_Invoke_SetTargetResource(
+    _In_opt_ LinuxOsConfigResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_opt_z_ const MI_Char* methodName,
+    _In_ const LinuxOsConfigResource* instanceName,
+    _In_opt_ const LinuxOsConfigResource_SetTargetResource* in)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(methodName);
+    MI_UNREFERENCED_PARAMETER(instanceName);
+    MI_UNREFERENCED_PARAMETER(in);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
