9253c7743936fd1a74f5682227a5d716da5480e4
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2023-09-19 11:01:30 -0700

    Work in progress

---
+++ src/adapters/mc2/codegen2.cmd
@@ -0,0 +1,9 @@
+REM copy OMI_error from admin\wmi\winomi\omiutils\mofs\OMI_Errors.mof, remove pragma and cim_error definition otherwise 'Stream' qualifier will be declared multiple times.
+REM copy CIM-2.2.60 CIM schema (http://www.dmtf.org/standards/cim/cim_schema_v2260) to ProviderGenerationTool folder.
+REM copy Convert-MofToprovider.exe to ProviderGenerationTool folder.
+REM use -OldRcPath to retain old rc resource numbers
+"convert-moftoprovider.exe" ^
+   -MofFile D:\ProviderGenerationTool\LinuxOsConfig.schema.mof ^
+   -ClassList LinuxOsConfigResource^
+   -IncludePath CIM-2.26.0 ^
+   -OutPath D:\ProviderGenerationTool\LinuxOsConfig\
