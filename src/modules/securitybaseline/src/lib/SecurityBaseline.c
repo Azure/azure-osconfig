b0793cd218782cce12b08ec74ec7231175dd16e7
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2023-09-29 14:47:20 -0700

    Work in progress

--- src/modules/securitybaseline/src/lib/SecurityBaseline.c
+++ src/modules/securitybaseline/src/lib/SecurityBaseline.c
@@ -2389,7 +2389,7 @@ static int RemediateEnsureAppropriateCiphersForSsh(void)
 static int RemediateEnsureAvahiDaemonServiceIsDisabled(void)
 {
     StopAndDisableDaemon(g_avahiDaemon, SecurityBaselineGetLog());
-    return AuditEnsureAvahiDaemonServiceIsDisabled() ? DuplicateString(g_fail) : DuplicateString(g_pass);
+    return (0 == strcmp(g_pass, AuditEnsureAvahiDaemonServiceIsDisabled())) ? 0 : ENOENT;
 }
 
 static int RemediateEnsureCupsServiceisDisabled(void)
@@ -2412,14 +2412,14 @@ static int RemediateEnsureRpcgssdServiceIsDisabled(void)
 {
     StopAndDisableDaemon(g_rpcgssd, SecurityBaselineGetLog());
     StopAndDisableDaemon(g_rpcGssd, SecurityBaselineGetLog());
-    return AuditEnsureRpcgssdServiceIsDisabled() ? DuplicateString(g_fail) : DuplicateString(g_pass);
+    return (0 == strcmp(g_pass, AuditEnsureRpcgssdServiceIsDisabled())) ? 0 : ENOENT;
 }
 
 static int RemediateEnsureRpcidmapdServiceIsDisabled(void)
 {
     StopAndDisableDaemon(g_rpcidmapd, SecurityBaselineGetLog());
     StopAndDisableDaemon(g_nfsIdmapd, SecurityBaselineGetLog());
-    return AuditEnsureRpcidmapdServiceIsDisabled() ? DuplicateString(g_fail) : DuplicateString(g_pass);
+    return (0 == strcmp(g_pass, AuditEnsureRpcidmapdServiceIsDisabled())) ? 0 : ENOENT;
 }
 
 static int RemediateEnsurePortmapServiceIsDisabled(void)
@@ -2427,13 +2427,13 @@ static int RemediateEnsurePortmapServiceIsDisabled(void)
     StopAndDisableDaemon(g_rpcbind, SecurityBaselineGetLog());
     StopAndDisableDaemon(g_rpcbindService, SecurityBaselineGetLog());
     StopAndDisableDaemon(g_rpcbindSocket, SecurityBaselineGetLog());
-    return AuditEnsurePortmapServiceIsDisabled() ? DuplicateString(g_fail) : DuplicateString(g_pass);
+    return (0 == strcmp(g_pass, AuditEnsurePortmapServiceIsDisabled() ? 0 : ENOENT;
 }
 
 static int RemediateEnsureNetworkFileSystemServiceIsDisabled(void)
 {
     StopAndDisableDaemon(g_nfsServer, SecurityBaselineGetLog());
-    return AuditEnsureNetworkFileSystemServiceIsDisabled();
+    return (0 == strcmp(g_pass, AuditEnsureNetworkFileSystemServiceIsDisabled())) ? 0 : ENOENT;
 }
 
 static int RemediateEnsureRpcsvcgssdServiceIsDisabled(void)
@@ -2444,19 +2444,19 @@ static int RemediateEnsureRpcsvcgssdServiceIsDisabled(void)
 static int RemediateEnsureSnmpServerIsDisabled(void)
 {
     StopAndDisableDaemon(g_snmpd, SecurityBaselineGetLog());
-    return AuditEnsureSnmpServerIsDisabled() ? DuplicateString(g_fail) : DuplicateString(g_pass);
+    return (0 == strcmp(g_pass, AuditEnsureSnmpServerIsDisabled())) ? 0 : ENOENT;
 }
 
 static int RemediateEnsureRsynServiceIsDisabled(void)
 {
     StopAndDisableDaemon(g_rsync, SecurityBaselineGetLog());
-    return AuditEnsureRsynServiceIsDisabled() ? DuplicateString(g_fail) : DuplicateString(g_pass);
+    return (0 == strcmp(g_pass, AuditEnsureRsynServiceIsDisabled())) ? 0 : ENOENT;
 }
 
 static int RemediateEnsureNisServerIsDisabled(void)
 {
     StopAndDisableDaemon(g_ypserv, SecurityBaselineGetLog());
-    return AuditEnsureNisServerIsDisabled() ? DuplicateString(g_fail) : DuplicateString(g_pass);
+    return (0 == strcmp(g_pass, AuditEnsureNisServerIsDisabled())) ? 0 : ENOENT;
 }
 
 static int RemediateEnsureRshClientNotInstalled(void)
