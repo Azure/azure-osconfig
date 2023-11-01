f142204618073e7aa8e559e31663a8279e3ed115
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2023-11-01 15:56:16 -0700

    Work in progress

--- src/common/commonutils/SshUtils.c
+++ src/common/commonutils/SshUtils.c
@@ -400,7 +400,9 @@ int CheckSshLoginGraceTime(char** reason, void* log)
 
 int SetSshOption(const char* option, const char* value, void* log)
 {
-    const char* commandTemplate = "sed -E 's/#%s\\s^\\w+(, \\w+)*$/.+?[\|$]/g/%s %s/;s/%s\\s^\\w+(, \\w+)*$/%s %s/' %s";
+    //\\w+(, \\w+)*$
+    //const char* commandTemplate = "sed -E 's/#%s\\s\\w+/.+?[\|$]/g/%s %s/;s/%s\\s\\w+/%s %s/' %s";
+    const char* commandTemplate = "sed -E 's/#%s\\s\\w+(, \\w+)*$/.+?[\|$]/g/%s %s/;s/%s\\s\\w+(, \\w+)*$/%s %s/' %s";
     const char* configurationBackup = "/etc/ssh/~sshd_config.bak";
 
     char* command = NULL;
