b2fc5b700c74894d76a09736c9ab81aa582ffb8e
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2023-12-11 12:15:23 -0800

    Better way

--- src/common/commonutils/SshUtils.c
+++ src/common/commonutils/SshUtils.c
@@ -942,11 +942,7 @@ int ProcessSshAuditCheck(const char* name, char* value, char** reason, void* log
         else
         {
             OsConfigLogError(log, "ProcessSshAuditCheck(%s): audit failure without a reason", name);
-            if (NULL == (*reason = DuplicateString(SECURITY_AUDIT_FAIL)))
-            {
-                OsConfigLogError(log, "ProcessSshAuditCheck: DuplicateString failed");
-                status = ENOMEM;
-            }
+            OsConfigCaptureReason(reason, "Audit failure without a reason, see /var/log/osconfig*", "%s, audit failure without a reason, see /var/log/osconfig*");
         }
     }
 
