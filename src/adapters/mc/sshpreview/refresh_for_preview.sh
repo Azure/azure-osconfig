3f9cf40b5a4450671496c075feed1d18917d583d
saicharithc <saicharithc@microsoft.com>
2024-02-15 12:58:17 -0800

    Added script to reinstall openssh & update osconfig

---
+++ src/adapters/mc/sshpreview/refresh_for_preview.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -n "$(command -v yum)" ]; then
+        echo "using yum"
+        yum remove openssh-server -y
+        rm /etc/ssh/sshd_config*
+        rm /etc/ssh/sshd_config.d/osconfig_*
+        yum install openssh-server -y
+
+        if [[ $(yum list installed | grep -wi osconfig) ]]; then
+                echo "OSConfig Installed, updating"
+                yum update
+                yum install osconfig
+        else
+                echo "OSConfig Not Present"
+        fi
+fi
+
+if [ -n "$(command -v apt-get)" ]; then
+        echo "using apt-get"
+        apt-get remove --purge openssh-server -y
+        rm /etc/ssh/sshd_config*
+        rm /etc/ssh/sshd_config.d/osconfig_*
+        apt-get install openssh-server -y
+
+        if [[ $(apt list osconfig | grep -wi osconfig) ]]; then
+                echo "OSConfig Installed, updating"
+                apt-get update
+                apt-get install osconfig
+        else
+                echo "OSConfig Not Present"
+        fi
+fi
