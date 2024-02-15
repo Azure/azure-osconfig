f86e7f02de64f228c657a9e8f0462cb3bfc0a533
saicharithc <saicharithc@microsoft.com>
2024-02-15 15:11:39 -0800

    removoed sudo from script

--- src/adapters/mc/sshpreview/refresh_for_preview.sh
+++ src/adapters/mc/sshpreview/refresh_for_preview.sh
@@ -2,15 +2,15 @@
 
 if [ -n "$(command -v yum)" ]; then
         echo "using yum"
-        sudo yum remove openssh-server -y
-        sudo rm /etc/ssh/sshd_config*
-        sudo rm /etc/ssh/sshd_config.d/osconfig_*
-        sudo yum install openssh-server -y
+        yum remove openssh-server -y
+        rm /etc/ssh/sshd_config*
+        rm /etc/ssh/sshd_config.d/osconfig_*
+        yum install openssh-server -y
 
         if [[ $(yum list installed | grep -wi osconfig) ]]; then
                 echo "OSConfig installed, updating"
-                sudo yum update
-                sudo yum install osconfig
+                yum update
+                yum install osconfig
         else
                 echo "OSConfig not installed"
         fi
@@ -18,15 +18,15 @@ fi
 
 if [ -n "$(command -v apt-get)" ]; then
         echo "using apt-get"
-        sudo apt-get remove --purge openssh-server -y
-        sudo rm /etc/ssh/sshd_config*
-        sudo rm /etc/ssh/sshd_config.d/osconfig_*
-        sudo apt-get install openssh-server -y
+        apt-get remove --purge openssh-server -y
+        rm /etc/ssh/sshd_config*
+        rm /etc/ssh/sshd_config.d/osconfig_*
+        apt-get install openssh-server -y
 
         if [[ $(apt list osconfig | grep -wi osconfig) ]]; then
                 echo "OSConfig installed, updating"
-                sudo apt-get update
-                sudo apt-get install osconfig
+                apt-get update
+                apt-get install osconfig
         else
                 echo "OSConfig not installed"
         fi
