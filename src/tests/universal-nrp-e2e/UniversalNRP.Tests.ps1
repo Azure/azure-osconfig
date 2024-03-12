d99f0390066bce84aa307273fbe0ad69c6371748
Ahmed Messaoud <ahbenmes@microsoft.com>
2024-03-12 13:05:19 -0700

    Adding E2E tests on Ubuntu 20.04 for the Universal NRP via the Machine Configuration PS cmdlets (#647)

---
+++ src/tests/universal-nrp-e2e/UniversalNRP.Tests.ps1
@@ -0,0 +1,73 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# UniversalNRP.Tests.ps1
+param (
+    [Parameter(Mandatory)]
+    [string] $PolicyPackage
+)
+
+Describe 'Validate Universal NRP' {
+    # Perform monitoring - Get
+    Context "Get" {
+        BeforeAll {
+            $result = Get-GuestConfigurationPackageComplianceStatus -Path $PolicyPackage -Verbose
+        }
+
+        It 'Ensure the total resource instances count' {    
+            $result.resources | Should -HaveCount 20
+        }
+
+        It 'Ensure resons are properly populated' {
+            foreach ($instance in $result.resources) {
+                if ($instance.properties.Reasons.Code -eq "PASS") {
+                    $instance.complianceStatus | Should -BeTrue
+                }
+                elseif ($instance.properties.Reasons.Code -eq "FAIL") {
+                    $instance.complianceStatus | Should -BeFalse
+                }
+                else {
+                    throw "Reasons are not properly populated"
+                }
+            }
+        }
+
+        It 'Ensure all resource instances have status' {    
+            $result.resources.complianceStatus | Should -Not -BeNullOrEmpty
+        }
+    }
+
+    # Perform Remediation - Set
+    Context "Set" {
+        BeforeAll {
+            Start-GuestConfigurationPackageRemediation -Path $PolicyPackage -Verbose
+            # Wait for remediation to complete
+            Start-Sleep -Seconds 60
+            $result = Get-GuestConfigurationPackageComplianceStatus -Path $PolicyPackage -Verbose
+        }
+
+        It 'Ensure the total resource instances count' {    
+            $result.resources | Should -HaveCount 20
+        }
+
+        It 'Ensure resons are properly populated' {
+            foreach ($instance in $result.resources) {
+                if ($instance.properties.Reasons.Code -eq "PASS") {
+                    $instance.complianceStatus | Should -BeTrue
+                }
+                elseif ($instance.properties.Reasons.Code -eq "FAIL") {
+                    throw "There should not be any failing resources after remediation"
+                }
+                else {
+                    throw "Reasons are not properly populated"
+                }
+            }
+        }
+
+        It 'Ensure all resource instances pass audit' {    
+            $result.resources.complianceStatus | Should -BeTrue
+        }
+
+        It 'Ensure overall audit pass' {
+            $result.complianceStatus | Should -BeTrue
+        }
+    }
+}
