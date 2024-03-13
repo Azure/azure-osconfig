81e31b8e8c7a31451d4f09ce060a496e7a665667
Robert Schaedler III <rschaedler@microsoft.com>
2024-03-13 17:12:36 -0400

    Add managed identity for self-hosted runner authentication (#650)
    * use managed identity for runner authentication
    
    * fix path to NRP zip

--- devops/e2e/terraform/iothub/providers.tf
+++ devops/e2e/terraform/iothub/providers.tf
@@ -9,9 +9,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
 }
