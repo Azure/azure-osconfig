2f3e293b63b3ce8b029d853145860a40d23cd1ef
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2024-05-12 11:35:16 -0700

    Work in progress

--- src/common/commonutils/MountUtils.c
+++ src/common/commonutils/MountUtils.c
@@ -399,7 +399,7 @@ int SetFileSystemMountingOption(const char* mountDirectory, const char* mountTyp
                     {
                         if (ConcatenateFiles(tempFileNameThree, tempFileNameTwo, log))
                         {
-                            rename(tempFileNameThree, tempFileNametwo);
+                            rename(tempFileNameThree, tempFileNameTwo);
                         }
                     }
                 }
