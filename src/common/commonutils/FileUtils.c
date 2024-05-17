90ad65f4efdf40b16c35600dd027b26da524a7c5
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2024-05-17 16:15:57 -0700

    Work in progress

--- src/common/commonutils/FileUtils.c
+++ src/common/commonutils/FileUtils.c
@@ -720,6 +720,7 @@ int ReplaceMarkedLinesInFile(const char* fileName, const char* marker, const cha
 
     FREE_MEMORY(tempFileName);
 
+    OsConfigLogInfo(log, "ReplaceMarkedLinesInFile('%s', '%s', '%s', '%c') complete with %d", fileName, marker, newline, commentCharacter, status);
     return status;
 }
 
