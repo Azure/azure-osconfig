bb3dd386638c70f627e4fbfb938144cea1287d94
Ahmed Messaoud <ahbenmes@microsoft.com>
2024-05-29 21:07:47 +0000

    Fixed Set

--- src/tests/universal-nrp-e2e/UniversalNRP.Tests.ps1
+++ src/tests/universal-nrp-e2e/UniversalNRP.Tests.ps1
@@ -13,37 +13,10 @@ param (
 Describe 'Validate Universal NRP' {
     # Perform Remediation - Set
     Context "Set" -Skip:$SkipRemediation {
-        BeforeAll {
+        It 'Perform remediation' {    
             Start-GuestConfigurationPackageRemediation -Path $PolicyPackage
             # Wait for remediation to complete
             Start-Sleep -Seconds 30
-            $result = Get-GuestConfigurationPackageComplianceStatus -Path $PolicyPackage
-        }
-
-        It 'Ensure the total resource instances count' {    
-            $result.resources | Should -HaveCount $ResourceCount
-        }
-
-        It 'Ensure resons are properly populated' {
-            foreach ($instance in $result.resources) {
-                if ($instance.properties.Reasons.Code -eq "PASS") {
-                    $instance.complianceStatus | Should -BeTrue
-                }
-                elseif ($instance.properties.Reasons.Code -eq "FAIL") {
-                    throw "There should not be any failing resources after remediation"
-                }
-                else {
-                    throw "Reasons are not properly populated"
-                }
-            }
-        }
-
-        It 'Ensure all resource instances pass audit' {    
-            $result.resources.complianceStatus | Should -BeTrue
-        }
-
-        It 'Ensure overall audit pass' {
-            $result.complianceStatus | Should -BeTrue
         }
     }
 
