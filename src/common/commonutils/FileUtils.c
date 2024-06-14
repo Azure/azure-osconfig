cf341c8be2aa21a2a3f1ab8a7d95b5a94859f0a9
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2024-06-14 11:49:38 -0700

    Work in progress

--- src/common/commonutils/FileUtils.c
+++ src/common/commonutils/FileUtils.c
@@ -53,6 +53,7 @@ char* LoadStringFromFile(const char* fileName, bool stopAtEol, void* log)
 static bool SaveToFile(const char* fileName, const char* mode, const char* payload, const int payloadSizeBytes, void* log)
 {
     FILE* file = NULL;
+    char* directory = NULL;
     int i = 0;
     bool result = true;
 
@@ -85,6 +86,11 @@ static bool SaveToFile(const char* fileName, const char* mode, const char* paylo
         {
             result = false;
             OsConfigLogError(log, "SaveToFile: cannot open '%s' in mode '%s' (%d)", fileName, mode, errno);
+
+            if ((NULL != (directory = dirname(fileName)) && (false == DirectoryExists(directory))))
+            {
+                OsConfigLogError(log, "SaveToFile: target directory '%s' does not exist", directory);
+            }
         }
     }
     else
