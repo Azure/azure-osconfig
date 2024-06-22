74cdb804aecb1d32235db5d63ed10a0d6c6ae37c
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2024-06-22 15:42:47 -0700

    Work in progress

--- src/common/asb/Asb.c
+++ src/common/asb/Asb.c
@@ -2664,7 +2664,7 @@ static int RemediateEnsureAuditdServiceIsRunning(char* value, void* log)
     else if ((false == CheckDaemonActive(g_auditd, NULL, log)) && (false == EnableAndStartDaemon(g_auditd, log)))
     {
         ExecuteCommand(NULL, "restorecon -r -v /var/log/audit", false, false, 0, 0, NULL, NULL, log);
-        if (false = StartDaemon(g_auditd, log))
+        if (false == StartDaemon(g_auditd, log))
         {
             status = ENOENT;
             for (i = 0; i < 3; i++)
