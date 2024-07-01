b33e903891bf4ec91ae1a9d10916d55094997607
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2024-07-01 14:15:02 -0700

    Work in progress

--- src/modules/configuration/tests/ConfigurationTests.cpp
+++ src/modules/configuration/tests/ConfigurationTests.cpp
@@ -277,7 +277,7 @@ TEST_F(ConfigurationTest, MmiSet)
         { m_desiredIotHubProtocolObject, "\"notImplemented\"", 22, m_iotHubProtocolObject, "\"mqttWebSocket\"" },
         { m_desiredGitManagementEnabledObject, "true", 0, m_gitManagementEnabledObject, "\"true\"" },
         { m_desiredGitManagementEnabledObject, "false", 0, m_gitManagementEnabledObject, "\"false\"" },
-        { m_desiredGitBranchObject, "\Foo\Test\", 0, m_gitBranchObject, "\"\\\"Foo\\/Test\\\"\"" }
+        { m_desiredGitBranchObject, "/Foo/Test/", 0, m_gitBranchObject, "/Foo/Test" }
     };
     
     int numTestCombinations = ARRAY_SIZE(testCombinations);
