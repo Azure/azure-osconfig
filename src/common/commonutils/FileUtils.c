31a5c47d051f96503c2359dba70eba0e122b7adb
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2024-10-17 10:15:18 -0700

    Work in progress

--- src/common/commonutils/FileUtils.c
+++ src/common/commonutils/FileUtils.c
@@ -77,7 +77,6 @@ static bool SaveToFile(const char* fileName, const char* mode, const char* paylo
         // See RestrictFileAccessToCurrentAccountOnly
         if (-1 != (fd = open(fileName, (0 == strcmp(mode, "a")) ? O_APPEND : O_CREAT, S_ISUID | S_ISGID | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IXUSR | S_IXGRP)))
         {
-            append = (0 == strcmp(mode, "a")) ? true : false;
             if (NULL != (file = fdopen(fd, mode)))
             {
                 if (true == (result = LockFile(file, log)))
