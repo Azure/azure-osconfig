998d6d2c26f1ed29692cc1af7f0e19e0063da963
Daniel Szot <danielszot@microsoft.com>
2024-11-20 15:33:01 +0100

    Add ASA scans running on CentOS 8. Lock base container images by SHA256.

--- devops/scripts/asa/run_asa.sh
+++ devops/scripts/asa/run_asa.sh
@@ -8,16 +8,33 @@ set -e
 # Tested and supported runtimes: podman, docker
 CONTAINER_RUNTIME="podman"
 
+DISTROS=("ubuntu22" "centos8")
+
+function echo_header {
+  echo ""
+  echo "============================================================="
+  echo ${1}
+  echo "============================================================="
+}
+
 if ! dpkg --list ${CONTAINER_RUNTIME} > /dev/null && ! rpm -q ${CONTAINER_RUNTIME} > /dev/null; then
     echo "Install prerequisites first: ${CONTAINER_RUNTIME}"
     exit 1
 fi
 
-${CONTAINER_RUNTIME} build -f ./Dockerfile-ubuntu22 -t osconfig-asa-ubuntu22 .
+for DISTRO in ${DISTROS[@]}; do
+    echo_header "RUNNING ${DISTRO} CONTAINER IMAGE BUILD"
 
-mkdir -p ./report
+    ${CONTAINER_RUNTIME} build -f ./Dockerfile-${DISTRO} -t osconfig-asa-${DISTRO} .
 
-${CONTAINER_RUNTIME} run -it \
-    -v ./scan.sh:/azure/azure-osconfig/devops/scripts/asa/scan.sh \
-    -v ./report:/azure/azure-osconfig/devops/scripts/asa/report \
-    osconfig-asa-ubuntu22 "$@"
+    echo_header "RUNNING ASA SCAN IN ${DISTRO} CONTAINER"
+    mkdir -p ./report/${DISTRO}
+
+    ${CONTAINER_RUNTIME} run -it \
+        -v ./scan.sh:/azure/azure-osconfig/devops/scripts/asa/scan.sh \
+        -v ./report/${DISTRO}:/azure/azure-osconfig/devops/scripts/asa/report \
+        osconfig-asa-${DISTRO} "$@"
+    echo_header "Finished for ${DISTRO}"
+done
+
+echo_header "No ASA problems detected. Scanned distros: ${DISTROS[@]}"
