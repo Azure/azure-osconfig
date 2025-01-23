2036030c67512f81deec772ccfccc1bb27a04a89
Ahmed Messaoud (from Dev Box) <ahbenmes@microsoft.com>
2025-01-23 18:38:23 +0000

    added az login

--- src/tests/universal-nrp-e2e/StartTests.sh
+++ src/tests/universal-nrp-e2e/StartTests.sh
@@ -90,8 +90,14 @@ echo "Downloading latest Azure Policy packages"
 mkdir -p $packageDir
 pipelineRunId=$(az pipelines runs list --organization $azdevopsOrg --project $azdevopsProject --pipeline-id $pipelineId --status completed --result succeeded --top 1 --query '[0].id' -o tsv)
 if [[ -z "$pipelineRunId" ]]; then
-    echo "Unable to retreive pipeline run id, ensure "az login" was performed or you have the correct access." 1>&2
-    exit 1
+    failedLogin=true
+    echo "Unable to retreive pipeline run id, running \"az login --subscription $subscriptionId\"" 1>&2
+    if ! az login --subscription $subscriptionId; then
+        echo "Failed to login to Azure. Please check your credentials and try again." 1>&2
+    else
+        failedLogin=false
+    fi
+    [[ "$failedLogin" = true ]] && { echo "Unable to retrieve pipeline run id after login attempt, please check credentials and try again." 1>&2; exit 1; }
 fi
 echo "Using latest succeeded run:$pipelineRunId"
 az pipelines runs artifact download --organization $azdevopsOrg --project $azdevopsProject --artifact-name $azdevopsArtifactName --path $packageDir --run-id $pipelineRunId
