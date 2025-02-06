e2031b4ccc843a3451f77280ef16785449dd5169
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2025-02-06 14:19:46 -0800

    Work in progress

--- src/adapters/mc/asb/Baseline.c
+++ src/adapters/mc/asb/Baseline.c
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "Common.h"
+#include "../Common.h"
 #include <Asb.h>
 
 int BaselineIsValidResourceIdRuleId(const char* resourceId, const char* ruleId, const char* payloadKey, void* log)
