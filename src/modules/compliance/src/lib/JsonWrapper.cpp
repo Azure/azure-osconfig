1a2cde42421cfcd490da4f0f8fa7268a20cb9af8
Robert Wojciechowski <robertwoj@microsoft.com>
2025-02-12 12:57:11 +0100

    Add missing files

---
+++ src/modules/compliance/src/lib/JsonWrapper.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "JsonWrapper.h"
+
+#include "parson.h"
+
+namespace compliance
+{
+    void JsonWrapperDeleter::operator()(json_value_t* value) const
+    {
+        json_value_free(value);
+    }
+
+    JsonWrapper parseJSON(const char* input)
+    {
+        return JsonWrapper(json_parse_string(input), JsonWrapperDeleter());
+    }
+} // namespace compliance
