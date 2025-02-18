718f8d7d518171dbacd1f45b830d9f2bee670dc2
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2025-02-18 10:20:57 -0800

    Work in progress

--- src/common/logging/Logging.h
+++ src/common/logging/Logging.h
@@ -34,11 +34,12 @@ char* GetFormattedTime();
 void TrimLog(OSCONFIG_LOG_HANDLE log);
 bool IsDaemon(void);
 
+#define __PREFIX_TEMPLATE__ "[%s][%s][%s:%d] "
 #define __SHORT_FILE__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-#define __LOG__(log, loglevel, format, ...) printf("[%s][%s][%s:%d]" format "\n", GetFormattedTime(), loglevel, __SHORT_FILE__, __LINE__, ## __VA_ARGS__)
+#define __LOG__(log, loglevel, format, ...) printf(__PREFIX_TEMPLATE__ format "\n", GetFormattedTime(), loglevel, __SHORT_FILE__, __LINE__, ## __VA_ARGS__)
 #define __LOG_TO_FILE__(log, loglevel, format, ...) {\
     TrimLog(log);\
-    fprintf(GetLogFile(log), "[%s][%s][%s:%d]" format "\n", GetFormattedTime(), loglevel, __SHORT_FILE__, __LINE__, ## __VA_ARGS__); \
+    fprintf(GetLogFile(log), __PREFIX_TEMPLATE__ format "\n", GetFormattedTime(), loglevel, __SHORT_FILE__, __LINE__, ## __VA_ARGS__); \
 }\
 
 #define __INFO__ "INFO"
