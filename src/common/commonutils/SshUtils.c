0892fa0b3f60d89ddd3ca20fe05ed7d5052d0d58
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2025-02-20 11:25:25 -0800

    Fix

--- src/common/commonutils/SshUtils.c
+++ src/common/commonutils/SshUtils.c
@@ -816,7 +816,7 @@ static int SetSshWarningBanner(unsigned int desiredBannerFileAccess, const char*
         if (0 != mkdir(etcAzSec, desiredBannerFileAccess))
         {
             status = errno ? errno : ENOENT;
-            OsConfigLogInfo(log, "SetSshWarningBanner: mkdir(%s, %u) failed with %d (errno: %d)", etcAzSec, desiredBannerFileAccess, status);
+            OsConfigLogInfo(log, "SetSshWarningBanner: mkdir(%s, %u) failed with %d (errno: %d)", etcAzSec, desiredBannerFileAccess, status, errno);
         }
     }
 
