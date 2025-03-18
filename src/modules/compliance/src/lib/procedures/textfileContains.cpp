06f92c7f494401e45c885eeb41582219943b00a3
Robert Wojciechowski <robertwoj@microsoft.com>
2025-03-18 14:54:43 +0100

    Introduce textfileContains compliance procedure

---
+++ src/modules/compliance/src/lib/procedures/textfileContains.cpp
@@ -0,0 +1,66 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+#include <CommonUtils.h>
+#include <Evaluator.h>
+#include <Result.h>
+#include <fstream>
+#include <regex>
+
+namespace compliance
+{
+AUDIT_FN(textfileContains)
+{
+    UNUSED(args);
+    UNUSED(logstream);
+    auto it = args.find("filePath");
+    if (it == args.end())
+    {
+        return Error("Missing 'filePath' parameter");
+    }
+    auto path = it->second;
+
+    it = args.find("matchPattern");
+    if (it == args.end())
+    {
+        return Error("Missing 'matchPattern' parameter");
+    }
+    auto pattern = it->second;
+
+    it = args.find("matchOperation");
+    auto matchOperation = it != args.end() ? it->second : std::string("pattern match");
+
+    std::ifstream file(path);
+    if (!file.is_open())
+    {
+        logstream << "Failed to open file" << std::endl;
+        return false;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+
+    if (matchOperation == "pattern match")
+    {
+        try
+        {
+            std::regex regex(pattern, std::regex_constants::extended);
+            if (std::regex_search(buffer.str(), std::move(regex)))
+            {
+                logstream << "pattern '" << std::move(pattern) << "' found in '" << path << "'";
+                return true;
+            }
+            else
+            {
+                logstream << "pattern '" << std::move(pattern) << "' not found in '" << path << "'";
+                return false;
+            }
+        }
+        catch (const std::exception& e)
+        {
+            return Error(std::string("Failed to match pattern: '") + pattern + "' " + e.what());
+        }
+    }
+
+    return Error(std::string("Unsupported operation '") + matchOperation + std::string("'"));
+}
+} // namespace compliance
