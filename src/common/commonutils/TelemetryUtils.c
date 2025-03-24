e366db6bd1d10a780481497e4274f761a8293d72
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2025-03-24 10:06:20 -0700

    Work in progress

--- src/common/commonutils/TelemetryUtils.c
+++ src/common/commonutils/TelemetryUtils.c
@@ -5,12 +5,8 @@
 
 void LogPerfClockTelemetry(PerfClock* clock, const char* targetName, const char* componentName, const char* objectName, int objectResult, const char* reason, OsConfigLogHandle log)
 {
-    const char* basicTelemetryTemplate = ",\"TargetName\":\"%s\",\"BaselineName\":\"%s\",\"Mode\":\"%s\",\"Seconds\":\"%.02f\"";
-    const char* failuresOrAllTelemetryTemplate = ",\"TargetName\":\"%s\",\"ComponentName\":\"%s\",\"ObjectName\":\"%s\",\"ObjectResult\":\"%s (%d)\",\"Microseconds\":\"%ld\"";
-    const char* debugTelemetryTemplate = ",\"TargetName\":\"%s\",\"ComponentName\":\"%s\",\"ObjectName\":\"%s\",\"ObjectResult\":\"%s (%d)\",\"Reason\":%s,\"Microseconds\":\"%ld\"";
-
-    long microseconds = -1;
     TelemetryLevel telemetryLevel = GetTelemetryLevel();
+    long microseconds = -1;
 
     if (NULL == clock)
     {
@@ -22,14 +18,17 @@ void LogPerfClockTelemetry(PerfClock* clock, const char* targetName, const char*
 
     if ((BasicTelemetry <= telemetryLevel) && (SESSIONS_TELEMETRY_MARKER == objectResult))
     {
-        OsConfigLogTelemetry(log, BasicTelemetry, basicTelemetryTemplate, targetName, componentName, objectName, microseconds / 1000000.0);
+        OsConfigLogTelemetry(log, BasicTelemetry, ",\"TargetName\":\"%s\",\"BaselineName\":\"%s\",\"Mode\":\"%s\",\"Seconds\":\"%.02f\"", targetName, componentName, objectName, microseconds / 1000000.0);
     }
     else if (DebugTelemetry > telemetryLevel)
     {
-        OsConfigLogTelemetry(log, objectResult ? FailuresTelemetry : AllTelemetry, failuresOrAllTelemetryTemplate, targetName, componentName, objectName, strerror(objectResult), objectResult, microseconds);
+        OsConfigLogTelemetry(log, objectResult ? FailuresTelemetry : AllTelemetry, 
+            ",\"TargetName\":\"%s\",\"ComponentName\":\"%s\",\"ObjectName\":\"%s\",\"ObjectResult\":\"%s (%d)\",\"Microseconds\":\"%ld\"", 
+            targetName, componentName, objectName, strerror(objectResult), objectResult, microseconds);
     }
     else if ((DebugTelemetry <= telemetryLevel) && (NULL != reason))
     {
-        OsConfigLogTelemetry(log, DebugTelemetry, debugTelemetryTemplate, targetName, componentName, objectName, strerror(objectResult), objectResult, reason, microseconds);
+        OsConfigLogTelemetry(log, DebugTelemetry, ",\"TargetName\":\"%s\",\"ComponentName\":\"%s\",\"ObjectName\":\"%s\",\"ObjectResult\":\"%s (%d)\",\"Reason\":%s,\"Microseconds\":\"%ld\"", 
+            targetName, componentName, objectName, strerror(objectResult), objectResult, reason, microseconds);
     }
 }
