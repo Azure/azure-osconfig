62ed8270d54b5fd2ce905c1fd210aa7507cdb471
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2025-03-24 10:14:18 -0700

    Work in progress

--- src/common/asb/Asb.c
+++ src/common/asb/Asb.c
@@ -654,7 +654,7 @@ static const long g_maxTotalTime = 1800000000;
 static char* g_prettyName = NULL;
 
 // Temporary hard-coded, normal default here must be NoTelemetry
-static TelemetryLevel g_telemetryLevel = BasicTelemetry; //NoTelemetry;
+static TelemetryLevel g_telemetryLevel = DebugTelemetry; //NoTelemetry;
 
 static bool g_auditOnly = true;
 
