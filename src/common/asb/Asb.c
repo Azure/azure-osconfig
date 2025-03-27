70d83efec7f947d0aebf9d512d0ec8e0ecc7ddaf
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2025-03-27 15:33:44 -0700

    Work in progress

--- src/common/asb/Asb.c
+++ src/common/asb/Asb.c
@@ -653,7 +653,7 @@ static const long g_maxTotalTime = 1800000000;
 
 static char* g_prettyName = NULL;
 
-static TelemetryLevel g_telemetryLevel = RequiredTelemetry;
+static TelemetryLevel g_telemetryLevel = OptionalTelemetry; //RequiredTelemetry;
 
 static bool g_auditOnly = true;
 
