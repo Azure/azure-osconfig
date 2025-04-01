c164137f88391d1aeeb165b9b34eab4cc6a25be0
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2025-04-01 10:20:25 -0700

    Removing unused definition

--- src/common/commonutils/CommonUtils.h
+++ src/common/commonutils/CommonUtils.h
@@ -247,7 +247,6 @@ int StartPerfClock(PerfClock* clock, OsConfigLogHandle log);
 int StopPerfClock(PerfClock* clock, OsConfigLogHandle log);
 long GetPerfClockTime(PerfClock* clock, OsConfigLogHandle log);
 void LogPerfClock(PerfClock* clock, const char* componentName, const char* objectName, int objectResult, long limit, OsConfigLogHandle log);
-void LogPerfClockTelemetry(PerfClock* clock, const char* targetName, const char* componentName, const char* objectName, int objectResult, const char* reason, OsConfigLogHandle log);
 
 #ifdef __cplusplus
 }
