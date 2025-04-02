8e3a3e3f2311271c61963b7386d728fe7f229bf1
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2025-04-01 18:43:48 -0700

    Work in progress

--- src/common/commonutils/PackageUtils.c
+++ src/common/commonutils/PackageUtils.c
@@ -133,9 +133,8 @@ static int CheckAllPackages(const char* commandTemplate, const char* packageMana
 
 static int ListAllInstalledPackages(OsConfigLogHandle log)
 {
-    const char* commandTemplate = "%s list installed";
-    const char* commandTemplateYumDnf = "%s list installed  --cacheonly";
     const char* commandTemplateDpkg = "%s-query -W -f='${binary:Package}\n'";
+    const char* commandTemplateYumDnf = "%s list installed  --cacheonly";
     const char* commandTmeplateZypper = "%s search --installed-only --query-format '%{name}\n'";
     char* results = NULL;
     int status = ENOENT;
