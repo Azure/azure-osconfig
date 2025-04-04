dce49a22d72cf23cae1a6d2d306d52bceb54f1c3
Robert Wojciechowski <robertwoj@microsoft.com>
2025-04-04 12:14:09 +0200

    Introduce fileRegexMatch compliance procedure (#916)

---
+++ src/modules/compliance/src/lib/procedures/fileRegexMatch.cpp
@@ -0,0 +1,171 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+#include <CommonUtils.h>
+#include <Evaluator.h>
+#include <Optional.h>
+#include <Regex.h>
+#include <Result.h>
+#include <fstream>
+namespace compliance
+{
+namespace
+{
+// In single pattern mode, we check if the pattern is present in the file.
+// The function returns true if the pattern matches any line in the file, false otherwise.
+bool SinglePatternMatchMode(std::ifstream& input, const std::string& matchPattern, std::regex_constants::syntax_option_type syntaxOptions, std::ostringstream& logstream)
+{
+    try
+    {
+        int lineNumber = 1;
+        auto matchRegex = regex(matchPattern, syntaxOptions);
+        std::string line;
+        while (std::getline(input, line))
+        {
+            if (regex_search(line, matchRegex))
+            {
+                logstream << "pattern '" << matchPattern << "' matched line " << std::to_string(lineNumber);
+                return true;
+            }
+
+            lineNumber++;
+        }
+    }
+    catch (const std::exception& e)
+    {
+        OsConfigLogError(nullptr, "Regex error: %s", e.what());
+        return false;
+    }
+
+    logstream << "pattern '" << matchPattern << "' did not match any line";
+    return false;
+}
+
+// In state-pattern match mode, we check each line of the input file if it matches the matchPattern regexp.
+// For each line that matches the main pattern, we check if statePattern regexp matches line matched by matchPattern.
+// The function returns true if the statePattern regexp matches all the lines that match the matchPattern regexp, false otherwise.
+bool StatePatternMatchMode(std::ifstream& input, const std::string& matchPattern, const std::string& statePattern,
+    std::regex_constants::syntax_option_type syntaxOptions, std::ostringstream& logstream)
+{
+    try
+    {
+        int lineNumber = 1;
+        auto matchRegex = regex(matchPattern, syntaxOptions);
+        auto stateRegex = regex(statePattern, syntaxOptions);
+        std::string line;
+        while (std::getline(input, line))
+        {
+            if (!regex_search(line, matchRegex))
+            {
+                lineNumber++;
+                continue;
+            }
+
+            if (!regex_search(line, stateRegex))
+            {
+                logstream << "state pattern '" << statePattern << "' not found in '" << line << "'";
+                return false;
+            }
+
+            lineNumber++;
+        }
+    }
+    catch (const std::exception& e)
+    {
+        OsConfigLogError(nullptr, "Regex error: %s", e.what());
+        return false;
+    }
+
+    return true;
+}
+} // anonymous namespace
+
+AUDIT_FN(fileRegexMatch, "filename:Path to the file to check:M", "matchOperation:Operation to perform on the file contents:M:^pattern match$",
+    "matchPattern:The pattern to match against the file contents:M",
+    "stateOperation:Operation to perform on each line that matches the 'matchPattern'::^pattern match$",
+    "statePattern:The pattern to match against each line that matches the 'statePattern'",
+    "caseSensitive:Determine whether the match should be case sensitive, applies to both 'matchPattern' and 'statePattern'::^true|false$")
+{
+    auto it = args.find("filename");
+    if (it == args.end())
+    {
+        return Error("Missing 'filename' parameter", EINVAL);
+    }
+    auto path = std::move(it->second);
+
+    it = args.find("matchPattern");
+    if (it == args.end())
+    {
+        return Error("Missing 'matchPattern' parameter", EINVAL);
+    }
+    auto matchPattern = std::move(it->second);
+
+    it = args.find("matchOperation");
+    if (it == args.end())
+    {
+        return Error("Missing 'matchOperation' parameter", EINVAL);
+    }
+    auto matchOperation = std::move(it->second);
+
+    it = args.find("stateOperation");
+    Optional<std::string> stateOperation;
+    if (it != args.end())
+    {
+        stateOperation = std::move(it->second);
+    }
+
+    it = args.find("statePattern");
+    Optional<std::string> statePattern;
+    if (it != args.end())
+    {
+        statePattern = std::move(it->second);
+    }
+    if (stateOperation.HasValue() && !statePattern.HasValue())
+    {
+        return Error("stateOperation field requires statePattern field", EINVAL);
+    }
+    if (!stateOperation.HasValue() && statePattern.HasValue())
+    {
+        return Error("statePattern field requires stateOperation field", EINVAL);
+    }
+
+    it = args.find("caseSensitive");
+    std::regex_constants::syntax_option_type syntaxOptions = std::regex_constants::extended;
+    if (it != args.end())
+    {
+        if (it->second == "false")
+        {
+            syntaxOptions |= std::regex_constants::icase;
+        }
+        else if (it->second != "true")
+        {
+            return Error("caseSensitive must be 'true' or 'false'", EINVAL);
+        }
+    }
+
+    std::ifstream file(path);
+    if (!file.is_open())
+    {
+        logstream << "Failed to open file" << std::endl;
+        return false;
+    }
+
+    if (matchOperation != "pattern match")
+    {
+        return Error(std::string("Unsupported operation '") + matchOperation + std::string("'"), EINVAL);
+    }
+
+    if (stateOperation.HasValue() && stateOperation.Value() != "pattern match")
+    {
+        return Error(std::string("Unsupported operation '") + stateOperation.Value() + std::string("'"), EINVAL);
+    }
+
+    if (!statePattern.HasValue())
+    {
+        return SinglePatternMatchMode(file, matchPattern, syntaxOptions, logstream);
+    }
+    else
+    {
+        return StatePatternMatchMode(file, matchPattern, statePattern.Value(), syntaxOptions, logstream);
+    }
+}
+} // namespace compliance
---
+++ src/modules/compliance/tests/procedures/fileRegexMatchTest.cpp
@@ -0,0 +1,258 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "Evaluator.h"
+#include "ProcedureMap.h"
+
+#include <dirent.h>
+#include <fstream>
+#include <gtest/gtest.h>
+#include <linux/limits.h>
+#include <string>
+#include <unistd.h>
+
+using compliance::Audit_fileRegexMatch;
+using compliance::Error;
+using compliance::Result;
+
+#include <iostream>
+class FileRegexMatchTest : public ::testing::Test
+{
+protected:
+    char mTempfile[PATH_MAX] = "/tmp/fileRegexMatchTest.XXXXXX";
+    int mTempfileFd = -1;
+    std::map<std::string, std::string> mArgs;
+    std::ostringstream mLogstream;
+
+    void SetUp() override
+    {
+        sprintf(mTempfile, "/tmp/fileRegexMatchTest.XXXXXX");
+        mTempfileFd = mkstemp(mTempfile);
+        ASSERT_TRUE(mTempfileFd != -1);
+    }
+
+    void TearDown() override
+    {
+        remove(mTempfile);
+        close(mTempfileFd);
+    }
+
+    void FillTempfile(const std::string& content)
+    {
+        std::ofstream file(mTempfile);
+        file << content;
+    }
+};
+
+TEST_F(FileRegexMatchTest, Audit_InvalidArguments_1)
+{
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(!result.HasValue());
+    ASSERT_EQ(result.Error().code, EINVAL);
+}
+
+TEST_F(FileRegexMatchTest, Audit_InvalidArguments_2)
+{
+    mArgs["filename"] = mTempfile;
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(!result.HasValue());
+    ASSERT_EQ(result.Error().code, EINVAL);
+}
+
+TEST_F(FileRegexMatchTest, Audit_InvalidArguments_3)
+{
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = "test";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    if (result.HasValue())
+    {
+        std::cerr << mLogstream.str() << std::endl;
+        std::cerr << "Result: " << result.Value() << std::endl;
+    }
+    ASSERT_TRUE(!result.HasValue());
+    ASSERT_EQ(result.Error().code, EINVAL);
+}
+
+TEST_F(FileRegexMatchTest, Audit_InvalidArguments_4)
+{
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = "test";
+    mArgs["matchOperation"] = "test"; // invalid match operation value
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(!result.HasValue());
+    ASSERT_EQ(result.Error().code, EINVAL);
+}
+
+TEST_F(FileRegexMatchTest, Audit_InvalidArguments_5)
+{
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = "(?i)"; // invalid regex pattern
+    mArgs["matchOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_FALSE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_EmptyFile_1)
+{
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = "test";
+    mArgs["matchOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), false);
+}
+
+TEST_F(FileRegexMatchTest, Audit_Match_1)
+{
+    FillTempfile("test");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = "test";
+    mArgs["matchOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_TRUE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_Match_2)
+{
+    FillTempfile("tests");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = "test";
+    mArgs["matchOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_TRUE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_Match_3)
+{
+    FillTempfile("test");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = "tests";
+    mArgs["matchOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_FALSE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_Match_4)
+{
+    FillTempfile("test");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = "te.t";
+    mArgs["matchOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_TRUE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_Match_5)
+{
+    FillTempfile("test");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = "^te.t$";
+    mArgs["matchOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_TRUE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_Match_6)
+{
+    FillTempfile(" \ttesting");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = R"(^[[:space:]]*te[a-z]t.*$)";
+    mArgs["matchOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_TRUE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_CaseInsensitive_1)
+{
+    FillTempfile(" \ttesTing");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = R"(^[[:space:]]*Te[a-z]t.*$)";
+    mArgs["matchOperation"] = "pattern match";
+    mArgs["caseSensitive"] = "false";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_TRUE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_State_1)
+{
+    FillTempfile("key=foo");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = R"(^key=.*$)";
+    mArgs["matchOperation"] = "pattern match";
+    mArgs["statePattern"] = R"(^key=foo$)";
+    mArgs["stateOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_TRUE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_State_2)
+{
+    FillTempfile("key=foo");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = R"(^key=.*$)";
+    mArgs["matchOperation"] = "pattern match";
+    mArgs["statePattern"] = R"(^key=bar$)";
+    mArgs["stateOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_FALSE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_Multiline_Match_2)
+{
+    FillTempfile("key=foo\nkey=bar\nkey=baz");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = R"(^key=.*$)";
+    mArgs["matchOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_TRUE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_Multiline_State_2)
+{
+    FillTempfile("key=foo\nkey=bar\nkey=baz");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = R"(^key=.*$)";
+    mArgs["matchOperation"] = "pattern match";
+    mArgs["statePattern"] = R"(^key=bar$)";
+    mArgs["stateOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_FALSE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_Multiline_State_3)
+{
+    FillTempfile("key=foo\nkey=bar\nkey=baz");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = R"(^key=.*$)";
+    mArgs["matchOperation"] = "pattern match";
+    mArgs["statePattern"] = R"(^key=(foo|bar|baz)$)";
+    mArgs["stateOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_TRUE(result.Value());
+}
+
+TEST_F(FileRegexMatchTest, Audit_Multiline_State_4)
+{
+    FillTempfile("key=foo\nkey=bar\nkey=baz");
+    mArgs["filename"] = mTempfile;
+    mArgs["matchPattern"] = R"(^key=.*$)";
+    mArgs["matchOperation"] = "pattern match";
+    mArgs["statePattern"] = R"(^key=(foo|bar)$)";
+    mArgs["stateOperation"] = "pattern match";
+    auto result = Audit_fileRegexMatch(mArgs, mLogstream);
+    ASSERT_TRUE(result.HasValue());
+    EXPECT_FALSE(result.Value());
+}
