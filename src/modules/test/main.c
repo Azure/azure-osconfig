7ca5a30beb0adb321313e7956ef2a9719ce38cee
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2025-04-23 17:30:45 -0700

    Work in progress

--- src/modules/test/main.c
+++ src/modules/test/main.c
@@ -365,11 +365,9 @@ int RunCommand(const COMMAND_STEP* command)
 
     if (command != NULL)
     {
-        // We do log and ignore the result of the command, this is not a MIM object, we want to just execute it
         if (command->status != (status = ExecuteCommand(NULL, command->arguments, false, false, 0, 0, &textResult, NULL, NULL)))
         {
             LOG_INFO("Command exited with status: %d (while we were expecting %d): %s", status, command->status, textResult);
-            status = 0;
         }
         else if (textResult != NULL)
         {
