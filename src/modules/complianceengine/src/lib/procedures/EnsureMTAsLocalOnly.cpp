3018a7dae9565bc78f0560ce42068e742e65532b
Witold Krecicki <witekkrecicki@microsoft.com>
2025-06-18 15:07:20 +0200

    Firewall-related audits (#1040)
    * Utility to list open ports, EnsureMTAsLocalOnly audit
    * Procedures for checking whether open ports are listed in the firewall

---
+++ src/modules/complianceengine/src/lib/procedures/EnsureMTAsLocalOnly.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "NetworkTools.h"
+
+#include <CommonUtils.h>
+#include <Evaluator.h>
+
+namespace ComplianceEngine
+{
+
+AUDIT_FN(EnsureMTAsLocalOnly)
+{
+    UNUSED(args);
+    auto result = GetOpenPorts(context);
+    if (!result.HasValue())
+    {
+        return result.Error();
+    }
+    const auto& openPorts = result.Value();
+    for (const auto& port : openPorts)
+    {
+        if (!port.IsLocal() && ((port.port == 25) || (port.port == 587) || (port.port == 465)))
+        {
+            return indicators.NonCompliant("MTA is listening on port " + std::to_string(port.port) + " on non-local interface");
+        }
+    }
+    return indicators.Compliant("No open MTA ports found on non-local interfaces");
+}
+
+} // namespace ComplianceEngine
