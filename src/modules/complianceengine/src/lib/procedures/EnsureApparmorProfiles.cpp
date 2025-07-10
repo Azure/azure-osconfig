19952d12f5d0b660082423f4a4e12385bcd6936f
Witold Krecicki <witekkrecicki@microsoft.com>
2025-07-10 13:57:58 +0200

    AppArmor profile procedure (#1073)
    * AppArmor profile procedure
    * Add ps -ef to the list of ExecuteCommandGrep allowed commands

---
+++ src/modules/complianceengine/src/lib/procedures/EnsureApparmorProfiles.cpp
@@ -0,0 +1,109 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include "../Evaluator.h"
+
+#include <CommonUtils.h>
+#include <iostream>
+#include <string>
+
+namespace ComplianceEngine
+{
+
+AUDIT_FN(EnsureApparmorProfiles, "enforce:Set for enforce (L2) mode, complain (L1) mode by default")
+{
+    std::string apparmorStatusCommand = "apparmor_status";
+
+    bool enforce = (args.find("enforce") != args.end());
+
+    Result<std::string> commandOutput = context.ExecuteCommand(apparmorStatusCommand);
+    if (!commandOutput.HasValue())
+    {
+        return indicators.NonCompliant("Failed to execute apparmor_status: " + commandOutput.Error().message);
+    }
+
+    int profilesEnforce = 0;
+    int profilesComplain = 0;
+    int profilesLoaded = 0;
+    int processesProfileUndefined = 0;
+    int profilesModeTotal = 0;
+    std::istringstream iss(commandOutput.Value());
+    std::string line;
+    while (std::getline(iss, line))
+    {
+        if (line.find("profiles are in enforce mode") != std::string::npos)
+        {
+            try
+            {
+                profilesEnforce = std::stoi(line.substr(0, line.find(' ')));
+            }
+            catch (const std::exception& e)
+            {
+                return Error(std::string("Failed to parse apparmor status output: ") + e.what(), EINVAL);
+            }
+        }
+        else if (line.find("profiles are in complain mode") != std::string::npos)
+        {
+            try
+            {
+                profilesComplain = std::stoi(line.substr(0, line.find(' ')));
+            }
+            catch (const std::exception& e)
+            {
+                return Error(std::string("Failed to parse apparmor status output: ") + e.what(), EINVAL);
+            }
+        }
+        else if (line.find("profiles are loaded") != std::string::npos)
+        {
+            try
+            {
+                profilesLoaded = std::stoi(line.substr(0, line.find(' ')));
+            }
+            catch (const std::exception& e)
+            {
+                return Error(std::string("Failed to parse apparmor status output: ") + e.what(), EINVAL);
+            }
+        }
+        else if (line.find("processes are unconfined but have a profile defined") != std::string::npos)
+        {
+            try
+            {
+                processesProfileUndefined = std::stoi(line.substr(0, line.find(' ')));
+            }
+            catch (const std::exception& e)
+            {
+                return Error(std::string("Failed to parse apparmor status output: ") + e.what(), EINVAL);
+            }
+        }
+    }
+
+    profilesModeTotal = profilesEnforce + profilesComplain;
+
+    if (0 == profilesLoaded)
+    {
+        return indicators.NonCompliant("No AppArmor profiles are loaded");
+    }
+
+    if (processesProfileUndefined > 0)
+    {
+        return indicators.NonCompliant("There are " + std::to_string(processesProfileUndefined) + " unconfined processes with a profile defined");
+    }
+
+    if (enforce)
+    {
+        if (profilesEnforce != profilesLoaded)
+        {
+            return indicators.NonCompliant("Not all loaded AppArmor profiles are in enforcing mode");
+        }
+    }
+    else
+    {
+        if (profilesModeTotal != profilesLoaded)
+        {
+            return indicators.NonCompliant("Not all loaded AppArmor profiles are in complain or enforcing mode");
+        }
+    }
+    return indicators.Compliant("AppArmor status command executed successfully");
+}
+
+} // namespace ComplianceEngine
---
+++ src/modules/complianceengine/tests/procedures/EnsureApparmorProfilesTest.cpp
@@ -0,0 +1,381 @@
+#include "Evaluator.h"
+#include "MockContext.h"
+#include "ProcedureMap.h"
+
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+
+using ComplianceEngine::AuditEnsureApparmorProfiles;
+using ComplianceEngine::Error;
+using ComplianceEngine::IndicatorsTree;
+using ComplianceEngine::Result;
+using ComplianceEngine::Status;
+using ::testing::Return;
+
+class EnsureApparmorProfilesTest : public ::testing::Test
+{
+protected:
+    MockContext mContext;
+    IndicatorsTree indicators;
+
+    void SetUp() override
+    {
+        indicators.Push("EnsureApparmorProfiles");
+    }
+};
+
+TEST_F(EnsureApparmorProfilesTest, AuditApparmorStatusCommandFails)
+{
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(Error("Command execution failed", -1))));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditNoProfilesLoaded)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "0 profiles are loaded.\n"
+        "0 profiles are in enforce mode.\n"
+        "0 profiles are in complain mode.\n"
+        "0 processes have profiles defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditUnconfinedProcessesWithProfileDefined)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "35 profiles are loaded.\n"
+        "16 profiles are in enforce mode.\n"
+        "5 profiles are in complain mode.\n"
+        "3 processes are unconfined but have a profile defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditComplainModeAllProfilesInComplainOrEnforce)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "35 profiles are loaded.\n"
+        "16 profiles are in enforce mode.\n"
+        "19 profiles are in complain mode.\n"
+        "10 processes have profiles defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::Compliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditComplainModeNotAllProfilesInComplainOrEnforce)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "35 profiles are loaded.\n"
+        "16 profiles are in enforce mode.\n"
+        "5 profiles are in complain mode.\n"
+        "10 processes have profiles defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditEnforceModeAllProfilesInEnforce)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "35 profiles are loaded.\n"
+        "35 profiles are in enforce mode.\n"
+        "0 profiles are in complain mode.\n"
+        "10 processes have profiles defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+    args["enforce"] = "";
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::Compliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditEnforceModeNotAllProfilesInEnforce)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "35 profiles are loaded.\n"
+        "30 profiles are in enforce mode.\n"
+        "5 profiles are in complain mode.\n"
+        "10 processes have profiles defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+    args["enforce"] = "";
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditEnforceModeWithComplainProfilesStillCompliant)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "35 profiles are loaded.\n"
+        "25 profiles are in enforce mode.\n"
+        "10 profiles are in complain mode.\n"
+        "10 processes have profiles defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+    args["enforce"] = "";
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditMixedOutputParsing)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "Some random line.\n"
+        "12 profiles are loaded.\n"
+        "Another irrelevant line.\n"
+        "8 profiles are in enforce mode.\n"
+        "4 profiles are in complain mode.\n"
+        "0 processes are unconfined but have a profile defined.\n"
+        "Some other data.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::Compliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditMinimalCompliantOutput)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "1 profiles are loaded.\n"
+        "0 profiles are in enforce mode.\n"
+        "1 profiles are in complain mode.\n"
+        "0 processes are unconfined but have a profile defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::Compliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditLargeNumbersInOutput)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "999 profiles are loaded.\n"
+        "500 profiles are in enforce mode.\n"
+        "499 profiles are in complain mode.\n"
+        "0 processes are unconfined but have a profile defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::Compliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditEmptyApparmorOutput)
+{
+    std::string apparmorOutput = "";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditOutputWithOnlyModuleInfo)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "Some other information.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditOutputWithPartialInformation)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "20 profiles are loaded.\n"
+        "10 profiles are in enforce mode.\n";
+    // Missing complain mode and unconfined processes lines
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditRealisticSampleOutput)
+{
+    // Based on the sample output provided by the user
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "35 profiles are loaded.\n"
+        "16 profiles are in enforce mode.\n"
+        "5 profiles are in complain mode.\n"
+        "10 processes have profiles defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant); // 16+5=21 < 35 loaded profiles
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditEnforceModeWithRealisticSample)
+{
+    // Based on the sample output provided by the user
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "35 profiles are loaded.\n"
+        "16 profiles are in enforce mode.\n"
+        "5 profiles are in complain mode.\n"
+        "10 processes have profiles defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+    args["enforce"] = "";
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant); // Only 16 out of 35 profiles in enforce mode
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditProfilesLoadedButNoModeSet)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "25 profiles are loaded.\n"
+        "0 profiles are in enforce mode.\n"
+        "0 profiles are in complain mode.\n"
+        "0 processes are unconfined but have a profile defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant); // 0+0=0 < 25 loaded profiles
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditEdgeCaseZeroLoadedProfilesAllowed)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "0 profiles are loaded.\n"
+        "0 profiles are in enforce mode.\n"
+        "0 profiles are in complain mode.\n"
+        "0 processes are unconfined but have a profile defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::NonCompliant); // No profiles loaded should be non-compliant
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditArgumentHandling)
+{
+    std::string apparmorOutput =
+        "apparmor module is loaded.\n"
+        "10 profiles are loaded.\n"
+        "10 profiles are in enforce mode.\n"
+        "0 profiles are in complain mode.\n"
+        "0 processes are unconfined but have a profile defined.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+    args["enforce"] = "true";       // Any value triggers enforce mode
+    args["otherParam"] = "ignored"; // Other parameters should be ignored
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::Compliant);
+}
+
+TEST_F(EnsureApparmorProfilesTest, AuditDifferentLineOrderings)
+{
+    std::string apparmorOutput =
+        "35 profiles are loaded.\n"
+        "apparmor module is loaded.\n"
+        "0 processes are unconfined but have a profile defined.\n"
+        "16 profiles are in enforce mode.\n"
+        "19 profiles are in complain mode.\n";
+
+    EXPECT_CALL(mContext, ExecuteCommand("apparmor_status")).WillOnce(Return(Result<std::string>(apparmorOutput)));
+
+    std::map<std::string, std::string> args;
+
+    auto result = AuditEnsureApparmorProfiles(args, indicators, mContext);
+    ASSERT_TRUE(result.HasValue());
+    ASSERT_EQ(result.Value(), Status::Compliant); // 16+19=35 equals loaded profiles
+}
