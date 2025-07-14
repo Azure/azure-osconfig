61ea622f4161d5872f008149bc690113bea8cf18
Robert Wojciechowski <robertwoj@microsoft.com>
2025-07-14 15:51:32 +0200

    TEST #15

--- devops/scripts/push-review-comments.sh
+++ devops/scripts/push-review-comments.sh
@@ -14,26 +14,29 @@ if [ ! -f "$diff_file" ]; then
   die "File $diff_file does not exist."
 fi
 
+LINE_NUM=0
 while IFS= read -r line; do
+    LINE_NUM=$((LINE_NUM + 1))
     if [[ "$line" == diff* ]]; then
         FILE=$(echo "$line" | cut -d' ' -f4 | sed 's|^b/||')
     elif [[ "$line" =~ \+*TODO* ]]; then
         COMMENT=$(echo "$line" | sed 's/^+//' | xargs)
-        ESCAPED_COMMENT=$(jq -Rn --arg str "$COMMENT" '$str')
-        LINE_NUM=$(grep -n "$line" "$diff_file" | head -n1 | cut -d: -f1)
+        ESCAPED_COMMENT=$(jq -r -n --arg str "$COMMENT" '$str')
         echo "[$FILE:$LINE_NUM] $COMMENT"
         body="{ \
-          \"body\": \"TEST COMMENT: \`TEST\`\", \
+          \"body\": \"TEST COMMENT: \`$ESCAPED_COMMENT\`\", \
           \"commit_id\": \"$COMMIT_SHA\", \
           \"path\": \"$FILE\", \
           \"line\": $LINE_NUM, \
           \"subject_type\": \"line\", \
           \"side\": \"RIGHT\" \
         }"
+        # mkdir -p "/tmp/$FILE"
+        # echo $body > "/tmp/$FILE/comment.json"
         url="https://api.github.com/repos/$REPO/pulls/$PR_NUMBER/comments"
-        echo "Posting comment to GitHub: $url"
-        echo "Body:"
-        echo "$body"
+        # echo "Posting comment to GitHub: $url"
+        # echo "Body:"
+        # echo "$body"
         curl -s -X POST \
             -H "Authorization: Bearer $GITHUB_TOKEN" \
             -H "Accept: application/vnd.github.v3+json" \
