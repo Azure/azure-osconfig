1de41b16a02e18a5ed4692141df31b6c26dd0b50
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2025-09-05 10:47:20 -0700

    Work in progress

--- src/common/asb/Asb.c
+++ src/common/asb/Asb.c
@@ -3832,12 +3832,16 @@ static int RemediateEnsureFilePermissionsForAllRsyslogLogFiles(char* value, OsCo
 
     InitEnsureFilePermissionsForAllRsyslogLogFiles(value);
 
+    OsConfigLogInfo(log, "g_desiredEnsureFilePermissionsForAllRsyslogLogFiles: '%s'", g_desiredEnsureFilePermissionsForAllRsyslogLogFiles); ////////////////////
+
     if (0 == (status = ConvertStringToIntegers(g_desiredEnsureFilePermissionsForAllRsyslogLogFiles, ',', &modes, &numberOfModes, 8, log)))
     {
         if (numberOfModes > 0)
         {
             if (NULL != (formattedMode = FormatAllocateString(formatTemplate, modes[numberOfModes - 1])))
             {
+                OsConfigLogInfo(log, "RemediateEnsureFilePermissionsForAllRsyslogLogFiles: <<< %s >>>", formattedMode); /////////////////////////
+                
                 status = SetEtcConfValue(g_etcRsyslogConf, g_fileCreateMode, formattedMode, log);
                 FREE_MEMORY(formattedMode);
             }
