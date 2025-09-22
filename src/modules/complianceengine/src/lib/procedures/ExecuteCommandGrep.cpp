6f8c0aa1f2a74b48061bf58613d46417aa43971e
Witold Krecicki <witekkrecicki@microsoft.com>
2025-09-22 12:21:09 +0200

    Use opendir/fts directory traverse instead of find to search filesystem for modules (#1133)
    * Use opendir/fts directory traverse instead of find to search filesystem for modules
    * Allow some new commands in ExecuteCommandGrep
    * RAII in KernelModuleTools

--- src/modules/complianceengine/src/lib/procedures/ExecuteCommandGrep.cpp
+++ src/modules/complianceengine/src/lib/procedures/ExecuteCommandGrep.cpp
@@ -12,7 +12,8 @@ namespace ComplianceEngine
 {
 
 static const std::set<std::string> allowedCommands = {"nft list ruleset", "nft list chain", "nft list tables", "ip6tables -L -n",
-    "ip6tables -L INPUT -v -n", "ip6tables -L OUTPUT -v -n", "iptables -L -n", "iptables -L INPUT -v -n", "iptables -L OUTPUT -v -n", "uname", "ps -ef"};
+    "ip6tables -L INPUT -v -n", "ip6tables -L OUTPUT -v -n", "iptables -L -n", "iptables -L INPUT -v -n", "iptables -L OUTPUT -v -n", "uname", "ps -ef",
+    "ps -eZ", "sestatus", "journalctl", "arch"};
 
 AUDIT_FN(ExecuteCommandGrep, "command:Command to be executed:M", "awk:Awk transformation in the middle, optional", "regex:Regex to be matched:M",
     "type:Type of regex, P for Perl (default) or E for Extended")
