d4690d1985819c6669dab8c6b943394d0bd5b9a2
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2025-09-29 13:31:02 -0700

    Work in progress

--- src/common/commonutils/FileUtils.c
+++ src/common/commonutils/FileUtils.c
@@ -1733,10 +1733,6 @@ char* GetStringOptionFromBuffer(const char* buffer, const char* option, char sep
     {
         OsConfigLogError(log, "GetStringOptionFromBuffer: failed to duplicate buffer string failed (%d)", errno);
     }
-    else if (commentCharacter == temp[0])
-    {
-        OsConfigLogInfo(log, "GetStringOptionFromBuffer: entire line is commented out");
-    }
     else if (NULL != (found = strstr(temp, option)))
     {
         TruncateAtFirst(found, commentCharacter);
