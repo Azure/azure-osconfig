c6845a05e3c1d553e706aa2be939338878d47e43
Ahmed Messaoud <ahbenmes@microsoft.com>
2025-10-02 01:10:45 +0000

    Addressed comments

--- src/modules/test/Common.h
+++ src/modules/test/Common.h
@@ -18,11 +18,11 @@
 
 #include <CommonUtils.h>
 #include <Logging.h>
-#include <Telemetry.h>
 #include <Reasons.h>
 #include <Asb.h>
 #include <Mmi.h>
 #include <version.h>
+#include <Telemetry.h>
 
 #define LOG_INFO(format, ...) OsConfigLogInfo(NULL, format, ##__VA_ARGS__)
 #define LOG_ERROR(format, ...) OsConfigLogError(NULL, format, ##__VA_ARGS__)
