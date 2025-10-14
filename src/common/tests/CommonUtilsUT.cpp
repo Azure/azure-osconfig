e29fb9a04ade148bf68033cd11d47609f5013434
Marius Niculescu <mariusni@microsoft.com>
2025-10-14 08:55:02 -0700

    Additional fixes for commonutils!EnumerateUserGroups (#1170)

--- src/common/tests/CommonUtilsUT.cpp
+++ src/common/tests/CommonUtilsUT.cpp
@@ -1510,6 +1510,12 @@ TEST_F(CommonUtilsTest, EnumerateUsersAndTheirGroups)
             EXPECT_NE(nullptr, groupList[j].groupName);
         }
 
+        if (0 == strcmp(userList[i].username, "testuser"))
+        {
+            EXPECT_EQ(1, groupListSize);
+            EXPECT_EQ("root", groupList[0].groupName);
+        }
+
         FreeGroupList(&groupList, groupListSize);
         EXPECT_EQ(nullptr, groupList);
     }
@@ -1522,6 +1528,9 @@ TEST_F(CommonUtilsTest, EnumerateAllGroups)
 {
     SimplifiedGroup* groupList = NULL;
     unsigned int groupListSize = 0;
+
+    FreeGroupList(&groupList, groupListSize);
+    EXPECT_EQ(nullptr, groupList);
 
     EXPECT_EQ(0, EnumerateAllGroups(&groupList, &groupListSize, nullptr, nullptr));
     EXPECT_EQ(groupListSize, GetNumberOfLinesInFile("/etc/group"));
