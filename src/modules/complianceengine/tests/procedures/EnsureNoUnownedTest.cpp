8824691075a20f27bc35cb28113d7fbc3dff9eb5
Witold Krecicki <witekkrecicki@microsoft.com>
2025-10-20 10:06:59 +0200

    Add filesystem scanner procedures to Lua (#1174)
    * ListDirectory procedure
    * FilesystemScanner minor fixes and cleanups
    * Lua procedure for getting filtered filesystem entries
    * More verbose Lua logging
    * Fix mask parsing, force octal values for masks in Lua
    * Add traceback to lua error

--- src/modules/complianceengine/tests/procedures/EnsureNoUnownedTest.cpp
+++ src/modules/complianceengine/tests/procedures/EnsureNoUnownedTest.cpp
@@ -28,8 +28,7 @@ protected:
         }
 
         indicators.Push("EnsureNoUnowned");
-        rootDir = mContext.GetTempdirPath();
-        rootDir += "/rootfs";
+        rootDir = mContext.GetFilesystemScannerRoot();
     }
 };
 
