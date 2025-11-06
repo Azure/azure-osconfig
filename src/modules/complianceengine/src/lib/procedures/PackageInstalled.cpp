a8528fc5ab01fd44f9c346adfff29ab5456d66b4
Witold Krecicki <witekkrecicki@microsoft.com>
2025-11-06 11:57:39 +0100

    Cache package manager detection too (#1200)
    * Cache package manager detection too
    * Use rpm -q, not rpm -qa, for package manager detection
    
    - relnote (bugfix) Improve package manager detection on RPM-based distros.

--- src/modules/complianceengine/src/lib/procedures/PackageInstalled.cpp
+++ src/modules/complianceengine/src/lib/procedures/PackageInstalled.cpp
@@ -61,13 +61,13 @@ Result<PackageManagerType> DetectPackageManager(ContextInterface& context)
     {
         return PackageManagerType::DPKG;
     }
-    output = context.ExecuteCommand("rpm -qa rpm");
+    output = context.ExecuteCommand("rpm -q rpm");
     if (output.HasValue())
     {
         return PackageManagerType::RPM;
     }
     // For SLES 15
-    output = context.ExecuteCommand("rpm -qa rpm-ndb");
+    output = context.ExecuteCommand("rpm -q rpm-ndb");
     if (output.HasValue())
     {
         return PackageManagerType::RPM;
@@ -452,8 +452,36 @@ Result<Status> AuditPackageInstalled(const PackageInstalledParams& params, Indic
     assert(params.packageManager.HasValue());
     assert(params.test_cachePath.HasValue());
     auto log = context.GetLogHandle();
+
+    PackageCache cache;
+    auto cacheResult = LoadPackageCache(params.test_cachePath.Value());
+    bool cacheValid = true;
+    bool cacheStale = false;
+    if (cacheResult.HasValue())
+    {
+        cache = cacheResult.Value();
+    }
+    else
+    {
+        cacheValid = false;
+        OsConfigLogInfo(log, "Failed to load package cache: %s", cacheResult.Error().message.c_str());
+    }
+
     auto packageManager = params.packageManager.Value();
-    if (packageManager == PackageManagerType::Autodetect)
+    if (cacheValid)
+    {
+        if (packageManager == PackageManagerType::Autodetect)
+        {
+            packageManager = cache.packageManager;
+        }
+        else if (cache.packageManager != packageManager)
+        {
+            cacheValid = false;
+            OsConfigLogInfo(log, "Package manager mismatch: expected %s, found %s", std::to_string(cache.packageManager).c_str(),
+                std::to_string(packageManager).c_str());
+        }
+    }
+    else if (packageManager == PackageManagerType::Autodetect)
     {
         auto result = DetectPackageManager(context);
         if (!result.HasValue())
@@ -472,26 +500,6 @@ Result<Status> AuditPackageInstalled(const PackageInstalledParams& params, Indic
     else
     {
         OsConfigLogInfo(log, "Checking if package %s is installed using package manager %s", params.packageName.c_str(), std::to_string(packageManager).c_str());
-    }
-
-    PackageCache cache;
-    auto cacheResult = LoadPackageCache(params.test_cachePath.Value());
-    bool cacheValid = true;
-    bool cacheStale = false;
-    if (cacheResult.HasValue())
-    {
-        cache = cacheResult.Value();
-    }
-    else
-    {
-        cacheValid = false;
-        OsConfigLogInfo(log, "Failed to load package cache: %s", cacheResult.Error().message.c_str());
-    }
-    if (cacheValid && (cache.packageManager != packageManager))
-    {
-        cacheValid = false;
-        OsConfigLogInfo(log, "Package manager mismatch: expected %s, found %s", std::to_string(cache.packageManager).c_str(),
-            std::to_string(packageManager).c_str());
     }
 
     if (cacheValid)
