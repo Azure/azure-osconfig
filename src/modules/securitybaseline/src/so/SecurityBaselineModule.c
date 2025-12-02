aa4ef6a8386de808c50084d6943056345f5da7c4
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2025-12-02 08:36:36 -0800

    Work in progress

--- src/modules/securitybaseline/src/so/SecurityBaselineModule.c
+++ src/modules/securitybaseline/src/so/SecurityBaselineModule.c
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include <CommonUtils.h>
+#include <Logging.h>
+
 #include <Mmi.h>
 #include <SecurityBaseline.h>
 
