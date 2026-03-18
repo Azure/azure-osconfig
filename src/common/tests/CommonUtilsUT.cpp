946925faaaeadb364e851c0b2c091c7461541646
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2026-03-18 13:37:58 -0700

    Work in progress

--- src/common/tests/CommonUtilsUT.cpp
+++ src/common/tests/CommonUtilsUT.cpp
@@ -3234,9 +3234,9 @@ TEST_F(CommonUtilsTest, LoggingOptions)
 TEST_F(CommonUtilsTest, CrashHandler)
 {
     // Pre-create the log file so the handler can open it
-    int testFile = -1
-
-    EXPECT_TRUE((0 < (testFile = open(m_path, O_WRONLY | O_CREAT | O_TRUNC, 0644))) ? true : false);
+    int testFile = open(m_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    
+    EXPECT_TRUE(testFile > 0);
 
     if (testFile > 0)
     {
