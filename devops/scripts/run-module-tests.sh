dd772942ffe46c11b8f340a70efe2dd18952eb71
Krzysztof Kanas <71030734+kkanas@users.noreply.github.com>
2026-03-26 11:18:09 +0100

    Fix for builds ./devops/scripts/run-module-tests.sh (#1261)
    * Initall fix for builds ./devops/scripts/run-module-tests.sh
    
    Signed-off-by: Krzysztof Kanas <kkanas@microsoft.com>
    
    * devops: fix containers Dockerfiles remove --from=dockerfiledir use path from source tree
    
    Remove Heredoc as it don't work with podman,
    fix script run-module-tests.sh to use Dockeerfiles and stop on error
    
    Signed-off-by: Krzysztof Kanas <kkanas@microsoft.com>
    
    ---------
    
    Signed-off-by: Krzysztof Kanas <kkanas@microsoft.com>

--- devops/scripts/run-module-tests.sh
+++ devops/scripts/run-module-tests.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
 
 usage()
 {
@@ -9,17 +10,15 @@ usage()
   exit 2
 }
 
-PARSED_ARGUMENTS=$(getopt -a -n $0 -o b:d:u:xn --long basedir:,distro:,builddir:,no-docker-build,no-build -- "$@")
-VALID_ARGUMENTS=$?
-
-if [ "$VALID_ARGUMENTS" != "0" ]; then
-  usage
-fi
+PARSED_ARGUMENTS=$(getopt -a -n "$0" -o b:d:u:xnh --long basedir:,distro:,builddir:,no-docker-build,no-build,help -- "$@")
 
 BASEDIR=`pwd`
-
+NODOCKERBUIILD=''
+DISTRO=''
+NOBUILD=''
+BUILDDIR=''
 eval set -- "$PARSED_ARGUMENTS"
-while :
+while true
 do
   case "$1" in
     -b | --basedir)          BASEDIR="$2"      ; shift 2 ;;
@@ -27,30 +26,41 @@ do
     -u | --builddir)         BUILDDIR="$2"       ; shift 2 ;;
     -x | --no-docker-build)  NODOCKERBUIILD=1  ; shift ;;
     -n | --no-build)         NOBUILD=1         ; shift ;;
+    -h | --help)             usage             ; shift ;;
     --) shift; break ;;
     *) echo "Unexpected option: $1 - this should not happen."
        usage ;;
   esac
 done
 
+if [ $# -ne 0 ] ; then
+    echo "Unexpected option: $1 - this should not happen."
+    usage
+fi
 
 if [ -z "$DISTRO" ]; then
-	DISTROS=$(ls -d $BASEDIR/devops/docker/*)
+    shopt -s globstar nullglob
+    DOCKERFILES=( "$BASEDIR"/devops/docker/**/Dockerfile )
 else
-	DISTROS="$BASEDIR/devops/docker/$DISTRO"
+    if [ ! -f "$BASEDIR/devops/docker/$DISTRO/Dockerfile" ] ; then
+      echo "Unabel to read $BASEDIR/devops/docker/$DISTRO/Dockerfile "
+      exit 1
+    fi
+    DOCKERFILES=( "$BASEDIR/devops/docker/$DISTRO/Dockerfile" )
 fi
 
 
-for DISTRO in $DISTROS; do
-  DISTRO_NAME=$(basename $DISTRO)
+for DOCKERFILE in "${DOCKERFILES[@]}"; do
+  DISTRO_NAME=${DOCKERFILE%/Dockerfile}
+  DISTRO_NAME=${DISTRO_NAME##${BASEDIR}/devops/docker/}
   IMGNAME=moduletest-$DISTRO_NAME:latest
-  if [ -z "$NODOCKERBUILD" ]; then
-  	docker build -t $IMGNAME $DISTRO
+  if [ -z "$NODOCKERBUIILD" ]; then
+    docker build -t "$IMGNAME" -f "$DOCKERFILE"  $PWD
   fi
   if [ -z "$BUILDDIR" ]; then
   	BDIR="build-$DISTRO_NAME"
   else
   	BDIR="$BUILDDIR"
   fi
-  docker run -v $BASEDIR:/git/azure-osconfig -w /git/azure-osconfig $IMGNAME ./devops/scripts/run-module-tests-int.sh $BDIR $NOBUILD
+  docker run -v "$BASEDIR:/git/azure-osconfig" -w /git/azure-osconfig "$IMGNAME" ./devops/scripts/run-module-tests-int.sh "$BDIR" "$NOBUILD"
 done
