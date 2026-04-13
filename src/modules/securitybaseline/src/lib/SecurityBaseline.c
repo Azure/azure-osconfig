17731e0d838d89fd4e1064cd11387b7409586bff
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2026-04-13 15:05:55 -0700

    Work in progress

--- src/modules/securitybaseline/src/lib/SecurityBaseline.c
+++ src/modules/securitybaseline/src/lib/SecurityBaseline.c
@@ -52,6 +52,7 @@ static OsConfigLogHandle SecurityBaselineGetLog(void)
 void SecurityBaselineInitialize(void)
 {
     g_log = OpenLog(g_securityBaselineLogFile, g_securityBaselineRolledLogFile);
+    TelemetryInitialize(log);
     CheckForPreviousCrash(g_securityBaselineLogFile, SecurityBaselineGetLog());
     InstallCrashHandler(g_securityBaselineLogFile);
     AsbInitialize(SecurityBaselineGetLog());
