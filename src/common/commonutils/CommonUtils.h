05d8aab66f68019b246b15f99246abd17d33bf81
Marius Niculescu <mariusni@microsoft.com>
2026-05-19 09:07:02 -0700

    Fix for ASB's 'Ensure lockout for failed password attempts is configured (CIS: L1 - Server - 5.3.2;') for RHEL 8 and similar (#1274)
    (cherry picked from commit b1d3b56f4f58232426d1609435914b7279fa248f)

--- src/common/commonutils/CommonUtils.h
+++ src/common/commonutils/CommonUtils.h
@@ -123,7 +123,10 @@ int CheckTextNotFoundInCommandOutput(const char* command, const char* text, char
 int SetEtcConfValue(const char* file, const char* name, const char* value, OsConfigLogHandle log);
 int SetEtcLoginDefValue(const char* name, const char* value, OsConfigLogHandle log);
 int CheckLockoutForFailedPasswordAttempts(const char* fileName, const char* pamSo, char commentCharacter, char** reason, OsConfigLogHandle log);
+int CheckLockoutForFailedPasswordAttemptsViaFaillockConf(const char* pamFile, const char* faillockConf, char** reason, OsConfigLogHandle log);
+int CheckPamFaillockModernModelInUse(const char* const* pamFiles, unsigned int numPamFiles, const char* faillockConf, OsConfigLogHandle log);
 int SetLockoutForFailedPasswordAttempts(OsConfigLogHandle log);
+int SetLockoutForFailedPasswordAttemptsViaFaillockConf(const char* faillockConf, int deny, int unlockTime, OsConfigLogHandle log);
 int CheckPasswordCreationRequirements(int retry, int minlen, int minclass, int dcredit, int ucredit, int ocredit, int lcredit, char** reason, OsConfigLogHandle log);
 int SetPasswordCreationRequirements(int retry, int minlen, int minclass, int dcredit, int ucredit, int ocredit, int lcredit, OsConfigLogHandle log);
 int CheckEnsurePasswordReuseIsLimited(int remember, char** reason, OsConfigLogHandle log);
