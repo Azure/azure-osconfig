b273bb87c752c40c8c709a3649023eb1728cc7e7
Krzysztof Kanas <kkanas@microsoft.com>
2026-06-03 15:50:24 +0200

    pre-commit add blank line to Behavior.h
    Signed-off-by: Krzysztof Kanas <kkanas@microsoft.com>

--- src/modules/complianceengine/src/lib/procedures/Behavior.h
+++ src/modules/complianceengine/src/lib/procedures/Behavior.h
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
 #ifndef COMPLIANCEENGINE_PROCEDURES_BEHAVIOR_H
 #define COMPLIANCEENGINE_PROCEDURES_BEHAVIOR_H
 
