ea2609109d9a0372546a383e5f10f9daba35c41c
Witek Krecicki <witekkrecicki@microsoft.com>
2026-06-12 14:54:16 +0000

    Remove stale non-complianceengine files

--- devops/e2e/terraform/iothub/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "iothub_name" {
-  value = azurerm_iothub.iothub.name
-}
-
-output "connection_string" {
-  value     = data.azurerm_iothub_shared_access_policy.iothubowner.primary_connection_string
-  sensitive = true
-}
