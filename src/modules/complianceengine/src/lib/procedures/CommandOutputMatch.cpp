e9abb2ba526c1f7872ca7e52185365fc776b5623
Robert Wojciechowski <robertwoj@microsoft.com>
2026-06-25 13:15:46 +0200

    Address code review issues #3

--- src/modules/complianceengine/src/lib/procedures/CommandOutputMatch.cpp
+++ src/modules/complianceengine/src/lib/procedures/CommandOutputMatch.cpp
@@ -12,8 +12,7 @@ namespace ComplianceEngine
 
 static const std::set<std::string> allowedCommands = {"nft list ruleset", "nft list chain", "nft list tables", "ip6tables -L -n",
     "ip6tables -L INPUT -v -n", "ip6tables -L OUTPUT -v -n", "iptables -L -n", "iptables -L INPUT -v -n", "iptables -L OUTPUT -v -n", "uname", "ps -ef",
-    "ps -eZ", "sestatus", "journalctl", "arch", "grubby --info=ALL", "pam-config --query --pwhistory", "getenforce", "ip link", "cat /etc/passwd",
-    "cat /etc/group"};
+    "ps -eZ", "sestatus", "journalctl", "arch", "grubby --info=ALL", "pam-config --query --pwhistory", "getenforce", "ip link"};
 
 Result<Status> AuditCommandOutputMatch(const CommandOutputMatchParams& params, IndicatorsTree& indicators, ContextInterface& context)
 {
