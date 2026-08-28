79610272f472f87124bc48ecd521aafdbf62d101
Witold Krecicki <witekkrecicki@microsoft.com>
2026-08-28 13:20:00 +0200

    Remove ComplianceEngine (#1297)
    * Remove ComplianceEngine build configuration and references
    
    Drop the ComplianceEngine module and assessor from the build (CMake), fuzzer, clang-tidy tooling, pre-commit hooks, and the devcontainer AugmentationEngine mount.
    
    * Remove ComplianceEngine documentation
    
    Drop ComplianceEngine references and the dedicated module section from README and the Copilot instructions.
    
    * Remove ComplianceEngine code
    
    Delete the ComplianceEngine module (src/modules/complianceengine), its Machine Configuration adapter (src/adapters/mc/complianceengine), and its module test recipes (src/modules/test/recipes/complianceengine).

--- src/modules/complianceengine/src/lib/GuestConfigurationContext.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-#include "GuestConfigurationContext.h"
-
-namespace ComplianceEngine
-{
-constexpr char GuestConfigurationContext::sStatePath[];
-} // namespace ComplianceEngine
