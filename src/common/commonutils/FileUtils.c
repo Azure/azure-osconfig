c36a87eecc5f667c567fa4db3e5324cc887e6359
Marius Niculescu (MariusNi) <mariusni@microsoft.com>
2026-09-08 09:47:57 -0700

    Work in progress

--- src/common/commonutils/FileUtils.c
+++ src/common/commonutils/FileUtils.c
@@ -1987,18 +1987,6 @@ int CheckIntegerOptionFromFileEqualWithAny(const char* fileName, const char* opt
         OsConfigCaptureReason(reason, "File '%s' not found or does not contain option '%s'", fileName, option);
     }
 
-    ////////////// HERE //////////////
-    OsConfigLogError(log, "TEMP: inducing a deliberate crash to test the crash handler");
-    {
-        int* crashInducer = NULL;
-        // Optimization barrier: hide the NULL from the compiler so it cannot prove the
-        // dereference (avoids -Wnull-dereference under -Werror) or delete the store.
-        __asm__ volatile("" : "+r"(crashInducer));
-        *crashInducer = 42;
-    }
-    //////////////////////////////////
-
-
     return result;
 }
 
